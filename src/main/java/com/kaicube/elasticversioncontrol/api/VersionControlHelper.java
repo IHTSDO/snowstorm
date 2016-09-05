79083d70bb7e12924abd70b56d9f9c596fcc9316
Kai Kewley <kaikewley@gmail.com>
2016-09-06 00:57:43 +0100

    Use streaming when ending and replacing repository entity versions.

--- src/main/java/com/kaicube/elasticversioncontrol/api/VersionControlHelper.java
+++ src/main/java/com/kaicube/elasticversioncontrol/api/VersionControlHelper.java
@@ -2,8 +2,8 @@ package com.kaicube.elasticversioncontrol.api;
 
 import com.kaicube.elasticversioncontrol.domain.Branch;
 import com.kaicube.elasticversioncontrol.domain.Commit;
-import com.kaicube.elasticversioncontrol.domain.Entity;
 import com.kaicube.elasticversioncontrol.domain.Component;
+import com.kaicube.elasticversioncontrol.domain.Entity;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.slf4j.Logger;
@@ -11,11 +11,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.data.elasticsearch.repository.ElasticsearchCrudRepository;
+import org.springframework.data.util.CloseableIterator;
 import org.springframework.util.Assert;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 
 import static org.elasticsearch.index.query.QueryBuilders.*;
@@ -76,8 +79,8 @@ public class VersionControlHelper {
 	}
 
 	<T extends Entity> void endOldVersions(Commit commit, String idField, Class<T> clazz, Collection<? extends Object> ids, ElasticsearchCrudRepository repository) {
-		// Find versions replaced across all paths
-		final List<T> localVersionsToEnd = elasticsearchTemplate.queryForList(new NativeSearchQueryBuilder()
+		// Find versions to end on this branch
+		final NativeSearchQuery componentsToEndQuery = new NativeSearchQueryBuilder()
 				.withQuery(
 						new BoolQueryBuilder()
 								.must(termQuery("path", commit.getFlatBranchPath()))
@@ -88,35 +91,43 @@ public class VersionControlHelper {
 						new BoolQueryBuilder()
 								.must(termsQuery(idField, ids))
 				)
-				.withPageable(new PageRequest(0, 10000)) // FIXME: this is temporary
-				.build(), clazz);
-		if (!localVersionsToEnd.isEmpty()) {
-			for (Entity replacedVersion : localVersionsToEnd) {
-				replacedVersion.setEnd(commit.getTimepoint());
-			}
-			repository.save(localVersionsToEnd);
+				.withPageable(new PageRequest(0, 10000))
+				.build();
+		List<Entity> componentVersionsToEnd = new ArrayList<>();
+		try (final CloseableIterator<T> localVersionsToEnd = elasticsearchTemplate.stream(componentsToEndQuery, clazz)) {
+			localVersionsToEnd.forEachRemaining(versionToEnd -> {
+				versionToEnd.setEnd(commit.getTimepoint());
+				componentVersionsToEnd.add(versionToEnd);
+			});
+		}
+		long componentVersionsEnded = componentVersionsToEnd.size();
+		if (!componentVersionsToEnd.isEmpty()) {
+			repository.save(componentVersionsToEnd);
+			componentVersionsToEnd.clear();
 		}
 
-		// Find versions to end across all paths
-		final List<? extends Entity> replacedVersions = elasticsearchTemplate.queryForList(new NativeSearchQueryBuilder().withQuery(
-				new BoolQueryBuilder()
-						.must(getBranchCriteria(commit.getBranch().getFatPath()))
-						.must(rangeQuery("start").lt(commit.getTimepoint()))
-						.mustNot(existsQuery("end"))
-		)
+		// Find versions replaced across all paths
+		final NativeSearchQuery componentsReplacedQuery = new NativeSearchQueryBuilder()
+				.withQuery(
+						new BoolQueryBuilder()
+								.must(getBranchCriteria(commit.getBranch().getFatPath()))
+								.must(rangeQuery("start").lt(commit.getTimepoint()))
+								.mustNot(existsQuery("end"))
+				)
 				.withFilter(
 						new BoolQueryBuilder()
 								.must(termsQuery(idField, ids))
 				)
-				.withPageable(new PageRequest(0, 10000)) // FIXME: this is temporary
-				.build(), clazz);
-		if (!replacedVersions.isEmpty()) {
-			for (Entity replacedVersion : replacedVersions) {
+				.withPageable(new PageRequest(0, 10000))
+				.build();
+		AtomicLong componentVersionsReplacedCount = new AtomicLong(0);
+		try (final CloseableIterator<T> componentVersionsReplaced = elasticsearchTemplate.stream(componentsReplacedQuery, clazz)) {
+			componentVersionsReplaced.forEachRemaining(replacedVersion -> {
 				commit.addVersionReplaced(replacedVersion.getInternalId());
-			}
-			repository.save(replacedVersions);
+				componentVersionsReplacedCount.incrementAndGet();
+			});
 		}
-		logger.info("{} old versions of {} replaced.", replacedVersions.size(), clazz);
+		logger.info(" - {} versions of {} replaced, {} ended on this branch.", componentVersionsReplacedCount.get(), clazz, componentVersionsEnded);
 	}
 
 	void setEntityMeta(Entity entity, Commit commit) {
