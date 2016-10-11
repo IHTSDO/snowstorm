69e6fcf9e56eef7d6b4c14094d3ca1f043fb871f
Kai Kewley <kaikewley@gmail.com>
2016-10-11 16:48:25 +0100

    Catch all exception handling.

--- src/main/java/com/kaicube/snomed/elasticsnomed/rest/ControllerAdvice.java
+++ src/main/java/com/kaicube/snomed/elasticsnomed/rest/ControllerAdvice.java
@@ -1,5 +1,7 @@
 package com.kaicube.snomed.elasticsnomed.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,12 +13,25 @@ import java.util.Map;
 @org.springframework.web.bind.annotation.ControllerAdvice
 public class ControllerAdvice {
 
+	private static final Logger logger = LoggerFactory.getLogger(ControllerAdvice.class);
+
 	@ExceptionHandler(IllegalArgumentException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public @ResponseBody
-	Map<String,Object> handleIndexNotFoundException(IllegalArgumentException exception) {
+	@ResponseBody
+	public Map<String,Object> handleIndexNotFoundException(IllegalArgumentException exception) {
 		HashMap<String, Object> result = new HashMap<>();
 		result.put("error", HttpStatus.BAD_REQUEST);
+		result.put("message", exception.getMessage());
+		return result;
+	}
+
+	@ExceptionHandler(Exception.class)
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	@ResponseBody
+	public Map<String,Object> handleException(Exception exception) {
+		logger.error("Unhandled exception.", exception);
+		HashMap<String, Object> result = new HashMap<>();
+		result.put("error", HttpStatus.INTERNAL_SERVER_ERROR);
 		result.put("message", exception.getMessage());
 		return result;
 	}
