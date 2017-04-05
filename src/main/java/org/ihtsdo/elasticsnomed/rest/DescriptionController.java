f59ca62360d21c95123ab612a9691405bd1c8036
Kai Kewley <kaikewley@gmail.com>
2017-04-05 11:48:57 +0100

    Remove description search pagination and change term param for compatibility with snow owl.

--- src/main/java/org/ihtsdo/elasticsnomed/rest/DescriptionController.java
+++ src/main/java/org/ihtsdo/elasticsnomed/rest/DescriptionController.java
@@ -7,7 +7,6 @@ import org.ihtsdo.elasticsnomed.domain.Description;
 import org.ihtsdo.elasticsnomed.rest.pojo.DescriptionSearchResult;
 import org.ihtsdo.elasticsnomed.services.ConceptService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,16 +25,19 @@ public class DescriptionController {
 	@RequestMapping(value = "browser/{branch}/descriptions", method = RequestMethod.GET)
 	@ResponseBody
 	@JsonView(value = View.Component.class)
-	public Page<DescriptionSearchResult> findConcepts(@PathVariable String branch, @RequestParam(required = false) String term,
+	public List<DescriptionSearchResult> findConcepts(@PathVariable String branch, @RequestParam(required = false) String query,
 													  @RequestParam(defaultValue = "0") int number, @RequestParam(defaultValue = "50") int size) {
-		org.springframework.data.domain.Page<Description> page = conceptService.findDescriptions(BranchPathUriUtil.parseBranchPath(branch), term, new PageRequest(number, size));
+		org.springframework.data.domain.Page<Description> page = conceptService.findDescriptions(BranchPathUriUtil.parseBranchPath(branch), query, new PageRequest(number, size));
 		Set<String> conceptIds = page.getContent().stream().map(Description::getConceptId).collect(Collectors.toSet());
 		Map<String, ConceptMini> conceptMinis = conceptService.findConceptMinis(branch, conceptIds);
 
 		List<DescriptionSearchResult> results = new ArrayList<>();
 		page.getContent().forEach(d -> results.add(new DescriptionSearchResult(d.getTerm(), d.isActive(), conceptMinis.get(d.getConceptId()))));
 
-		return new Page<>(new PageImpl<>(results, new PageRequest(number, size), page.getTotalElements()));
+		// TODO - update snow-owl to use pagination
+		// return new Page<>(new PageImpl<>(results, new PageRequest(number, size), page.getTotalElements()));
+
+		return results;
 	}
 
 }
