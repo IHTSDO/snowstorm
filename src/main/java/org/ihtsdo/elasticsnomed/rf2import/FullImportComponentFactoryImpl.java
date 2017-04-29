96bac51c5d3f6f235cd0e69ca65fb745cc5a2107
Kai Kewley <kaikewley@gmail.com>
2017-04-29 20:33:16 +0100

    Import: Log release branch creation during Full import.

--- src/main/java/org/ihtsdo/elasticsnomed/rf2import/FullImportComponentFactoryImpl.java
+++ src/main/java/org/ihtsdo/elasticsnomed/rf2import/FullImportComponentFactoryImpl.java
@@ -3,11 +3,14 @@ package org.ihtsdo.elasticsnomed.rf2import;
 import io.kaicode.elasticvc.api.BranchService;
 import org.ihtsdo.elasticsnomed.services.ConceptService;
 import org.ihtsdo.otf.snomedboot.factory.HistoryAwareComponentFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FullImportComponentFactoryImpl extends ImportComponentFactoryImpl implements HistoryAwareComponentFactory {
 
 	private String basePath;
 	private final String stopImportAfterEffectiveTime;
+	private Logger logger = LoggerFactory.getLogger(getClass());
 
 	public FullImportComponentFactoryImpl(ConceptService conceptService, BranchService branchService, String path, String stopImportAfterEffectiveTime) {
 		super(conceptService, branchService, path);
@@ -23,7 +26,9 @@ public class FullImportComponentFactoryImpl extends ImportComponentFactoryImpl i
 	@Override
 	public void loadingReleaseDeltaFinished(String releaseDate) {
 		completeImportCommit();
-		getBranchService().create(basePath + "/" + releaseDate);
+		String releaseBranchPath = basePath + "/" + releaseDate;
+		logger.info("Creating release branch {}", releaseBranchPath);
+		getBranchService().create(releaseBranchPath);
 		if (stopImportAfterEffectiveTime != null && stopImportAfterEffectiveTime.equals(releaseDate)) {
 			throw new RuntimeException("Stopping import here after " + stopImportAfterEffectiveTime);
 		}
