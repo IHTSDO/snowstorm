8ba579a8d1839b72daa2ff765d636bd4a6f64904
Kai Kewley <kaikewley@gmail.com>
2017-09-11 23:39:02 +0100

    CIS: Remove Service annotation from IdentifierCacheManager.

--- src/main/java/org/ihtsdo/elasticsnomed/core/data/services/identifier/IdentifierCacheManager.java
+++ src/main/java/org/ihtsdo/elasticsnomed/core/data/services/identifier/IdentifierCacheManager.java
@@ -15,7 +15,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
 public class IdentifierCacheManager implements Runnable {
 
 	// Time between successive polls in minutes
