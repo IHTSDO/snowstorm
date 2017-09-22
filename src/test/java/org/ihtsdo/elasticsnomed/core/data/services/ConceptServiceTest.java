fcfcbea8b58ae30d30abba22e40cff841e84151e
Kai Kewley <kaikewley@gmail.com>
2017-09-22 12:55:55 +0100

    Revert "Move 10K unit test to build profile "bulk-tests"."
    This reverts commit 519827614f9e097ed50379a9bd8eb95188cb7777.

--- src/test/java/org/ihtsdo/elasticsnomed/core/data/services/ConceptServiceTest.java
+++ src/test/java/org/ihtsdo/elasticsnomed/core/data/services/ConceptServiceTest.java
@@ -3,6 +3,7 @@ package org.ihtsdo.elasticsnomed.core.data.services;
 import io.kaicode.elasticvc.api.BranchService;
 import org.ihtsdo.elasticsnomed.TestConfig;
 import org.ihtsdo.elasticsnomed.core.data.domain.*;
+import org.ihtsdo.elasticsnomed.core.data.services.pojo.ResultMapPage;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -33,6 +34,9 @@ public class ConceptServiceTest {
 
 	@Autowired
 	private DescriptionService descriptionService;
+
+	@Autowired
+	private RelationshipService relationshipService;
 
 	@Autowired
 	private ReferenceSetMemberService referenceSetMemberService;
@@ -598,6 +602,61 @@ public class ConceptServiceTest {
 		conceptWithRestoredDate = conceptService.find(conceptId, path);
 		ReferenceSetMember memberWithRestoredDate = conceptWithRestoredDate.getDescriptions().iterator().next().getLangRefsetMembers().values().iterator().next();
 		assertEquals(effectiveTime, memberWithRestoredDate.getEffectiveTime());
+	}
+
+	@Test
+	public void testCreateUpdate10KConcepts() throws ServiceException {
+		branchService.create("MAIN/A");
+		conceptService.create(new Concept(SNOMEDCT_ROOT), "MAIN/A");
+
+		List<Concept> concepts = new ArrayList<>();
+		final int tenThousand = 10 * 1000;
+		for (int i = 0; i < tenThousand; i++) {
+			concepts.add(
+					new Concept(null, Concepts.CORE_MODULE)
+							.addDescription(new Description("Concept " + i))
+							.addDescription(new Description("Concept " + i + "(finding)"))
+							.addRelationship(new Relationship(Concepts.ISA, SNOMEDCT_ROOT))
+			);
+		}
+
+		final Iterable<Concept> conceptsCreated = conceptService.create(concepts, "MAIN/A");
+
+		final Page<Concept> page = conceptService.findAll("MAIN/A", new PageRequest(0, 100));
+		assertEquals(concepts.size() + 1, page.getTotalElements());
+		assertEquals(Concepts.CORE_MODULE, page.getContent().get(50).getModuleId());
+
+		ResultMapPage<String, ConceptMini> conceptDescendants = conceptService.findConceptDescendants(SNOMEDCT_ROOT, "MAIN/A", Relationship.CharacteristicType.stated, new PageRequest(0, 50));
+		assertEquals(10 * 1000, conceptDescendants.getTotalElements());
+
+		List<Relationship> inboundRelationships = relationshipService.findInboundRelationships(SNOMEDCT_ROOT, "MAIN/A", Relationship.CharacteristicType.stated);
+		assertEquals(10 * 1000, inboundRelationships.size());
+
+		final String anotherModule = "123123";
+		List<Concept> toUpdate = new ArrayList<>();
+		conceptsCreated.forEach(concept -> {
+			concept.setModuleId(anotherModule);
+			toUpdate.add(concept);
+		});
+
+		conceptService.createUpdate(toUpdate, "MAIN/A");
+
+		final Page<Concept> pageAfterUpdate = conceptService.findAll("MAIN/A", new PageRequest(0, 100));
+		assertEquals(tenThousand + 1, pageAfterUpdate.getTotalElements());
+		Concept someConcept = pageAfterUpdate.getContent().get(50);
+		if (someConcept.getId().equals(SNOMEDCT_ROOT)) {
+			someConcept = pageAfterUpdate.getContent().get(51);
+		}
+		assertEquals(anotherModule, someConcept.getModuleId());
+		assertEquals(1, someConcept.getRelationships().size());
+
+		// Move all concepts in hierarchy
+		conceptService.create(new Concept(Concepts.CLINICAL_FINDING).addRelationship(new Relationship(ISA, SNOMEDCT_ROOT)), "MAIN/A");
+		concepts.forEach(c -> {
+			c.getRelationships().iterator().next().setActive(false);
+			c.addRelationship(new Relationship(ISA, Concepts.CLINICAL_FINDING));
+		});
+		conceptService.createUpdate(concepts, "MAIN/A");
 	}
 
 	private void printAllDescriptions(String path) {
