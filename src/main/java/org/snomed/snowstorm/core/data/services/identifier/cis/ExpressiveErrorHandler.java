6424243b39780d585967dbf254872b59a228b88b
Kai Kewley <kaikewley@gmail.com>
2018-06-01 22:55:36 +0100

    Add logging to Rest client error handler.

--- src/main/java/org/snomed/snowstorm/core/data/services/identifier/cis/ExpressiveErrorHandler.java
+++ src/main/java/org/snomed/snowstorm/core/data/services/identifier/cis/ExpressiveErrorHandler.java
@@ -1,5 +1,7 @@
 package org.snomed.snowstorm.core.data.services.identifier.cis;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.DefaultResponseErrorHandler;
@@ -11,6 +13,9 @@ import java.io.InputStreamReader;
 import java.util.stream.Collectors;
 
 public class ExpressiveErrorHandler extends DefaultResponseErrorHandler {
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
+
 	@Override
 	public void handleError(ClientHttpResponse response) {
 		// Recover error code and message from response
@@ -27,6 +32,8 @@ public class ExpressiveErrorHandler extends DefaultResponseErrorHandler {
 		} catch (IOException ignored) {
 			errMsg = "Unable to recover failure reason";
 		}
+
+		logger.info("Got REST client error {} - {}", statusCode, statusText);
 
 		throw new RestClientResponseException(errMsg, statusCode, statusText, null, null, null);
 	}
