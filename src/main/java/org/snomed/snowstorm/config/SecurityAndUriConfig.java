b618fac54dddd9b72421345d7fbb45c4151b1bc9
Kai Kewley <kaikewley@gmail.com>
2018-07-16 17:18:46 +0100

    STORM-101 Correct description controller mapping.

--- src/main/java/org/snomed/snowstorm/config/SecurityAndUriConfig.java
+++ src/main/java/org/snomed/snowstorm/config/SecurityAndUriConfig.java
@@ -65,6 +65,7 @@ public class SecurityAndUriConfig extends WebSecurityConfigurerAdapter {
 				"/(.*)/concepts",
 				"/(.*)/concepts/.*",
 				"/(.*)/relationships.*",
+				"/(.*)/descriptions.*",
 				"/(.*)/members.*",
 				"/(.*)/classifications.*",
 				"/(.*)/integrity-check",
