3defe722d8424960f42427ef66a7696861cd3baa
Kai Kewley <kaikewley@gmail.com>
2018-12-05 20:35:05 +0000

    Add "Snowstorm startup complete" message.

--- src/main/java/org/snomed/snowstorm/SnowstormApplication.java
+++ src/main/java/org/snomed/snowstorm/SnowstormApplication.java
@@ -61,6 +61,8 @@ public class SnowstormApplication extends Config implements ApplicationRunner {
 			mrcmService.loadFromFiles();
 			referenceSetMemberService.init();
 
+			logger.info("--- Snowstorm startup complete ---");
+
 			if (applicationArguments.containsOption(IMPORT_ARG)) {
 				// Import a single release or 'Snapshot' from an Edition RF2 zip file from disk at startup
 				String releasePath = getOneValue(applicationArguments, IMPORT_ARG);
