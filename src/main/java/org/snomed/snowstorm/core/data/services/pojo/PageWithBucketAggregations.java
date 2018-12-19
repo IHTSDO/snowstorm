28d925aa57395d10aa7c13d9054a905e49ab7825
Kai Kewley <kaikewley@gmail.com>
2018-12-19 21:23:30 +0000

    STORM-99 Add concept minis for each description aggregation bucket.

--- src/main/java/org/snomed/snowstorm/core/data/services/pojo/PageWithBucketAggregations.java
+++ src/main/java/org/snomed/snowstorm/core/data/services/pojo/PageWithBucketAggregations.java
@@ -1,5 +1,8 @@
 package org.snomed.snowstorm.core.data.services.pojo;
 
+import com.fasterxml.jackson.annotation.JsonView;
+import org.snomed.snowstorm.core.data.domain.ConceptMini;
+import org.snomed.snowstorm.rest.View;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 
@@ -9,14 +12,24 @@ import java.util.Map;
 public class PageWithBucketAggregations<T> extends PageImpl<T> {
 
 	private Map<String, Map<String, Long>> buckets;
+	private Map<String, ConceptMini> bucketConcepts;
 
 	public PageWithBucketAggregations(List<T> content, Pageable pageable, long total, Map<String, Map<String, Long>> buckets) {
 		super(content, pageable, total);
 		this.buckets = buckets;
 	}
 
+	@JsonView(value = View.Component.class)
 	public Map<String, Map<String, Long>> getBuckets() {
 		return buckets;
 	}
 
+	public void setBucketConcepts(Map<String, ConceptMini> bucketConcepts) {
+		this.bucketConcepts = bucketConcepts;
+	}
+
+	@JsonView(value = View.Component.class)
+	public Map<String, ConceptMini> getBucketConcepts() {
+		return bucketConcepts;
+	}
 }
