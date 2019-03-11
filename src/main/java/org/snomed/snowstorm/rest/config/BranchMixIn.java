b55514b97802b67ac7b4e672aa14f30082931cda
Kai Kewley <kaikewley@gmail.com>
2019-03-11 14:28:28 +0000

    STORM-160 Branch JSON property sorting for readability.

--- src/main/java/org/snomed/snowstorm/rest/config/BranchMixIn.java
+++ src/main/java/org/snomed/snowstorm/rest/config/BranchMixIn.java
@@ -2,12 +2,15 @@ package org.snomed.snowstorm.rest.config;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import io.kaicode.elasticvc.domain.Entity;
 
 import java.util.Date;
 import java.util.Map;
 import java.util.Set;
 
+@JsonPropertyOrder(value = {"path", "state", "containsContent", "locked", "creation", "base", "head",
+		"creationTimestamp", "baseTimestamp", "headTimestamp", "metadata", "versionsReplacedCounts"})
 public abstract class BranchMixIn {
 
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = Entity.DATE_FORMAT_STRING)
