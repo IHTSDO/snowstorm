a0df2f2815a07233fd868df922af522abea7f524
CoderMChu <mch@ihtsdo.org>
2019-03-15 15:31:39 +0000

    OWLAP-37: Add query to exclude deleted version of axioms when searching for defined definition status

--- src/main/java/org/snomed/snowstorm/core/data/services/ConceptDefinitionStatusUpdateService.java
+++ src/main/java/org/snomed/snowstorm/core/data/services/ConceptDefinitionStatusUpdateService.java
@@ -115,6 +115,7 @@ public class ConceptDefinitionStatusUpdateService extends ComponentService imple
                         .must(prefixQuery(ReferenceSetMember.Fields.getAdditionalFieldKeywordTypeMapping(
                                 ReferenceSetMember.OwlExpressionFields.OWL_EXPRESSION), DEFINED_CLASS_AXIOM_PREFIX))
                         .must(termsQuery(ReferenceSetMember.Fields.REFERENCED_COMPONENT_ID, concepts))
+                        .mustNot(termsQuery(ReferenceSetMember.Fields.MEMBER_ID, commit.getEntityVersionsDeleted()))
                 )
                 .withFields(ReferenceSetMember.Fields.REFERENCED_COMPONENT_ID)
                 .withPageable(ConceptService.LARGE_PAGE).build(), ReferenceSetMember.class)) {
