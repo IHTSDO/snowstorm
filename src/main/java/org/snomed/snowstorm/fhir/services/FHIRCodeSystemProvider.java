05b36a98c1fb6f1116b7857f3f01934dd48722e3
Peter Groves Williams <petergwilliams@hotmail.com>
2019-03-29 11:47:20 +0000

    FHIR Remove Accept-Language for now, incompatible annotation

--- src/main/java/org/snomed/snowstorm/fhir/services/FHIRCodeSystemProvider.java
+++ src/main/java/org/snomed/snowstorm/fhir/services/FHIRCodeSystemProvider.java
@@ -3,13 +3,12 @@ package org.snomed.snowstorm.fhir.services;
 import ca.uhn.fhir.rest.annotation.Operation;
 import ca.uhn.fhir.rest.annotation.OperationParam;
 import ca.uhn.fhir.rest.server.IResourceProvider;
+
 import org.hl7.fhir.dstu3.model.*;
 import org.hl7.fhir.dstu3.model.OperationOutcome.IssueType;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.snomed.snowstorm.core.data.domain.CodeSystemVersion;
 import org.snomed.snowstorm.core.data.domain.Concept;
-import org.snomed.snowstorm.core.data.domain.ConceptMini;
-import org.snomed.snowstorm.core.data.domain.Relationship;
 import org.snomed.snowstorm.core.data.services.CodeSystemService;
 import org.snomed.snowstorm.core.data.services.ConceptService;
 import org.snomed.snowstorm.core.data.services.NotFoundException;
@@ -18,13 +17,10 @@ import org.snomed.snowstorm.fhir.config.FHIRConstants;
 import org.snomed.snowstorm.rest.ControllerHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestHeader;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Collection;
 import java.util.List;
 
 import static io.kaicode.elasticvc.api.ComponentService.LARGE_PAGE;
@@ -55,15 +51,18 @@ public class FHIRCodeSystemProvider implements IResourceProvider, FHIRConstants 
 			@OperationParam(name="version") StringType codeSystemUri,
 			@OperationParam(name="coding") Coding coding,
 //				@OperationParam(name="date") DateTimeType date,   // Not supported
-			@OperationParam(name="property") List<CodeType> properties,
-			@RequestHeader(value = "Accept-Language", defaultValue = ControllerHelper.DEFAULT_ACCEPT_LANG_HEADER) String acceptLanguageHeader) throws FHIROperationException {
+			@OperationParam(name="property") List<CodeType> properties
+			/*@RequestHeader(value = "Accept-Language", defaultValue = ControllerHelper.DEFAULT_ACCEPT_LANG_HEADER) String acceptLanguageHeader*/) throws FHIROperationException {
 
 		if (system == null || system.isEmpty() || !system.equals(SNOMED_URI)) {
 			throw new FHIROperationException(IssueType.VALUE, "System must be present, and currently only " + SNOMED_URI + " is supported.");
 		}
 
 		String defaultModule = helper.getSnomedEditionModule(codeSystemUri);
-		Integer editionVersionString = helper.getSnomedVersion(codeSystemUri.toString());
+		Integer editionVersionString = null;
+		if (codeSystemUri != null) {
+			editionVersionString = helper.getSnomedVersion(codeSystemUri.toString());
+		}
 
 		org.snomed.snowstorm.core.data.domain.CodeSystem codeSystem = codeSystemService.findByDefaultModule(defaultModule);
 		if (codeSystem == null) {
@@ -84,9 +83,10 @@ public class FHIRCodeSystemProvider implements IResourceProvider, FHIRConstants 
 		}
 		String branchPath = codeSystemVersion.getBranchPath();
 
-		List<String> languageCodes = ControllerHelper.getLanguageCodes(acceptLanguageHeader);
-
-		Concept concept = ControllerHelper.throwIfNotFound("Concept", conceptService.find(code.getValue(), languageCodes, branchPath));
+		//List<String> languageCodes = ControllerHelper.getLanguageCodes(acceptLanguageHeader);
+		List<String> languageCodes = ControllerHelper.getLanguageCodes(ControllerHelper.DEFAULT_ACCEPT_LANG_HEADER);
+		
+				Concept concept = ControllerHelper.throwIfNotFound("Concept", conceptService.find(code.getValue(), languageCodes, branchPath));
 		Page<Long> childIds = queryService.searchForIds(queryService.createQueryBuilder(false).ecl("<!" + code.getValue()), branchPath, LARGE_PAGE);
 		return mapper.mapToFHIR(concept, childIds.getContent());
 	}
