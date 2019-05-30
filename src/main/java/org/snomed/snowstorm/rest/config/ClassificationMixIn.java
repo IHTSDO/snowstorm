04ac0d7a77361dcdb77ee0acc7ec61f2a41e9386
Kai Kewley <kaikewley@gmail.com>
2019-05-30 21:13:15 +0100

    OWLAP-187 Fix classification job date formats - remove miliseconds.

--- src/main/java/org/snomed/snowstorm/rest/config/ClassificationMixIn.java
+++ src/main/java/org/snomed/snowstorm/rest/config/ClassificationMixIn.java
@@ -1,22 +1,23 @@
 package org.snomed.snowstorm.rest.config;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import io.kaicode.elasticvc.domain.Entity;
 
 import java.util.Date;
 
 public abstract class ClassificationMixIn {
 
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = Entity.DATE_FORMAT_STRING)
+	private static final String DATE_FORMAT_STRING = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DATE_FORMAT_STRING)
 	abstract Date getCreationDate();
 
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = Entity.DATE_FORMAT_STRING)
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DATE_FORMAT_STRING)
 	abstract Date getCompletionDate();
 
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = Entity.DATE_FORMAT_STRING)
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DATE_FORMAT_STRING)
 	abstract Date getLastCommitDate();
 
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = Entity.DATE_FORMAT_STRING)
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DATE_FORMAT_STRING)
 	abstract Date getSaveDate();
 
 }
