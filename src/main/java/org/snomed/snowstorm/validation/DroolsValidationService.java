119bb7e81ef2a5af958e0cd34af705eb8c4cd586
Kai Kewley <kaikewley@gmail.com>
2019-09-24 12:36:55 +0100

    OWLAP-458 Populate drools effectiveTimes from the store.
    Co-Authored-By: quyenly87 <quocquyenit@gmail.com>

--- src/main/java/org/snomed/snowstorm/validation/DroolsValidationService.java
+++ src/main/java/org/snomed/snowstorm/validation/DroolsValidationService.java
@@ -5,6 +5,8 @@ import io.kaicode.elasticvc.api.BranchCriteria;
 import io.kaicode.elasticvc.api.BranchService;
 import io.kaicode.elasticvc.api.VersionControlHelper;
 import io.kaicode.elasticvc.domain.Branch;
+import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap;
+import it.unimi.dsi.fastutil.longs.LongArrayList;
 import org.ihtsdo.drools.RuleExecutor;
 import org.ihtsdo.drools.RuleExecutorFactory;
 import org.ihtsdo.drools.response.InvalidContent;
@@ -13,6 +15,8 @@ import org.ihtsdo.otf.resourcemanager.ResourceManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.snomed.snowstorm.core.data.domain.Concept;
+import org.snomed.snowstorm.core.data.domain.Description;
+import org.snomed.snowstorm.core.data.domain.Relationship;
 import org.snomed.snowstorm.core.data.services.BranchMetadataKeys;
 import org.snomed.snowstorm.core.data.services.DescriptionService;
 import org.snomed.snowstorm.core.data.services.QueryService;
@@ -22,11 +26,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.data.util.CloseableIterator;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
 import java.util.*;
 import java.util.stream.Collectors;
+
+import static io.kaicode.elasticvc.api.VersionControlHelper.LARGE_PAGE;
+import static java.lang.Long.parseLong;
+import static org.elasticsearch.index.query.QueryBuilders.*;
 
 @Service
 public class DroolsValidationService {
@@ -85,11 +96,67 @@ public class DroolsValidationService {
 		BranchCriteria branchCriteria = versionControlHelper.getBranchCriteria(branchWithInheritedMetadata);
 		Set<DroolsConcept> droolsConcepts = concepts.stream().map(DroolsConcept::new).collect(Collectors.toSet());
 
+		// Look-up release hashes from the store to set/update the component effectiveTimes
+		setReleaseHashAndEffectiveTime(concepts);
+
 		ConceptDroolsValidationService conceptService = new ConceptDroolsValidationService(branchPath, branchCriteria, elasticsearchOperations, queryService);
 		DescriptionDroolsValidationService descriptionService = new DescriptionDroolsValidationService(branchPath, branchCriteria, versionControlHelper, elasticsearchOperations,
 				this.descriptionService, queryService, testResourceProvider);
 		RelationshipDroolsValidationService relationshipService = new RelationshipDroolsValidationService(branchCriteria, elasticsearchOperations);
 		return ruleExecutor.execute(ruleSetNames, droolsConcepts, conceptService, descriptionService, relationshipService, false, false);
+	}
+
+	private void setReleaseHashAndEffectiveTime(Set<Concept> concepts) {
+		Map<Long, Concept> conceptMap = new Long2ObjectOpenHashMap<>();
+		Map<Long, Description> descriptionMap = new Long2ObjectOpenHashMap<>();
+		Map<Long, Relationship> relationshipMap = new Long2ObjectOpenHashMap<>();
+		concepts.forEach(concept -> {
+			conceptMap.put(concept.getConceptIdAsLong(), concept);
+			concept.getDescriptions().forEach(description -> descriptionMap.put(parseLong(description.getId()), description));
+			concept.getRelationships().forEach(relationship -> relationshipMap.put(parseLong(relationship.getId()), relationship));
+		});
+		try (CloseableIterator<Concept> conceptStream = elasticsearchOperations.stream(new NativeSearchQueryBuilder()
+				.withQuery(boolQuery()
+						.must(termQuery(Concept.Fields.RELEASED, true))
+						.must(termsQuery(Concept.Fields.CONCEPT_ID, conceptMap.keySet()))
+				)
+				.withPageable(LARGE_PAGE)
+				.build(), Concept.class)) {
+			conceptStream.forEachRemaining(conceptFromStore -> {
+				Concept concept = conceptMap.get(conceptFromStore.getConceptIdAsLong());
+				concept.setReleasedEffectiveTime(conceptFromStore.getReleasedEffectiveTime());
+				concept.setReleaseHash(conceptFromStore.getReleaseHash());
+				concept.updateEffectiveTime();
+			});
+		}
+		try (CloseableIterator<Description> descriptionStream = elasticsearchOperations.stream(new NativeSearchQueryBuilder()
+				.withQuery(boolQuery()
+						.must(termQuery(Concept.Fields.RELEASED, true))
+						.must(termsQuery(Description.Fields.DESCRIPTION_ID, descriptionMap.keySet()))
+				)
+				.withPageable(LARGE_PAGE)
+				.build(), Description.class)) {
+			descriptionStream.forEachRemaining(descriptionFromStore -> {
+				Description description = descriptionMap.get(parseLong(descriptionFromStore.getId()));
+				description.setReleasedEffectiveTime(descriptionFromStore.getReleasedEffectiveTime());
+				description.setReleaseHash(descriptionFromStore.getReleaseHash());
+				description.updateEffectiveTime();
+			});
+		}
+		try (CloseableIterator<Relationship> relationshipsFromStore = elasticsearchOperations.stream(new NativeSearchQueryBuilder()
+				.withQuery(boolQuery()
+						.must(termQuery(Concept.Fields.RELEASED, true))
+						.must(termsQuery(Relationship.Fields.RELATIONSHIP_ID, relationshipMap.keySet()))
+				)
+				.withPageable(LARGE_PAGE)
+				.build(), Relationship.class)) {
+			relationshipsFromStore.forEachRemaining(relationshipFromStore -> {
+				Relationship relationship = relationshipMap.get(parseLong(relationshipFromStore.getId()));
+				relationship.setReleasedEffectiveTime(relationshipFromStore.getReleasedEffectiveTime());
+				relationship.setReleaseHash(relationshipFromStore.getReleaseHash());
+				relationship.updateEffectiveTime();
+			});
+		}
 	}
 
 	public void newRuleExecutorAndResources() {
