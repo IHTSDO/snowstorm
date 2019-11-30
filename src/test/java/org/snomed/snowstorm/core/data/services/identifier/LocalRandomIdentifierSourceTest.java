75042963adafd9ff9d7e1d488bd8afdf9289b04c
Kai Kewley <kaikewley@gmail.com>
2019-11-30 21:07:20 +0000

    MAINT-1081 Check random generator does generate dupes within a batch.

--- src/test/java/org/snomed/snowstorm/core/data/services/identifier/LocalRandomIdentifierSourceTest.java
+++ src/test/java/org/snomed/snowstorm/core/data/services/identifier/LocalRandomIdentifierSourceTest.java
@@ -1,5 +1,6 @@
 package org.snomed.snowstorm.core.data.services.identifier;
 
+import it.unimi.dsi.fastutil.longs.LongOpenHashSet;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.snomed.snowstorm.AbstractTest;
@@ -15,6 +16,7 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TestConfig.class)
@@ -44,7 +46,6 @@ public class LocalRandomIdentifierSourceTest extends AbstractTest {
 
 			// We now know which identifiers will be generated
 			List<Long> longs = identifierSource.reserveIds(0, "10", 3);
-			System.out.println(longs);
 			assertEquals("100108", longs.get(0).toString());
 			assertEquals("101107", longs.get(1).toString());
 			assertEquals("102104", longs.get(2).toString());
@@ -52,7 +53,6 @@ public class LocalRandomIdentifierSourceTest extends AbstractTest {
 			// And can reset the counter
 			itemId.set(100);
 			longs = identifierSource.reserveIds(0, "10", 3);
-			System.out.println(longs);
 			assertEquals("100108", longs.get(0).toString());
 			assertEquals("101107", longs.get(1).toString());
 			assertEquals("102104", longs.get(2).toString());
@@ -63,12 +63,44 @@ public class LocalRandomIdentifierSourceTest extends AbstractTest {
 			// Reset counter and generated again..
 			itemId.set(100);
 			longs = identifierSource.reserveIds(0, "10", 3);
-			System.out.println(longs);
+			assertEquals(3, longs.size());
 			assertEquals("100108", longs.get(0).toString());
 			// We now don't get 101107 generated again even though the counter was reset
 			// because this number is already in the store.
 			assertEquals("102104", longs.get(1).toString());
 			assertEquals("103105", longs.get(2).toString());
+		} finally {
+			identifierSource.setItemIdProvider(originalItemIdProvider);
+		}
+	}
+
+	@Test
+	public void testGenerate10KWithClashDetection() throws ServiceException {
+		LocalRandomIdentifierSource.ItemIdProvider originalItemIdProvider = identifierSource.getItemIdProvider();
+		try {
+			// Replace random ids with sequential based on 100
+			// in order to simulate generating an identifier which is already used.
+			AtomicInteger itemId = new AtomicInteger(100);
+			identifierSource.setItemIdProvider(() -> itemId.getAndIncrement() + "");
+
+			// We now know which identifiers will be generated
+			int quantity = 5_512;
+			List<Long> longs = identifierSource.reserveIds(0, "10", quantity);
+			assertEquals(quantity, longs.size());
+			assertEquals("100108", longs.get(0).toString());
+			int offset = quantity - 1;
+			assertEquals("5611103", longs.get(offset).toString());
+
+			// Create a concept with one of these identifiers
+			Long id = longs.get(4_000);
+			conceptService.create(new Concept(id.toString()).addFSN("Test"), "MAIN");
+
+			// Reset counter and generated again..
+			itemId.set(100);
+			longs = identifierSource.reserveIds(0, "10", 10_000);
+			assertEquals(10_000, new LongOpenHashSet(longs).size());
+			assertEquals("100108", longs.get(0).toString());
+			assertNotEquals("5611103", longs.get(offset).toString());
 		} finally {
 			identifierSource.setItemIdProvider(originalItemIdProvider);
 		}
