f4459908c2a49fc1c6fd447c6021f746ac480149
Kai Kewley <kaikewley@gmail.com>
2019-12-09 14:58:06 +0000

    INFRA-4461 Admin function to reset inactive relationship group numbers.

--- src/main/java/org/snomed/snowstorm/core/data/services/AdminOperationsService.java
+++ src/main/java/org/snomed/snowstorm/core/data/services/AdminOperationsService.java
@@ -1,19 +1,26 @@
 package org.snomed.snowstorm.core.data.services;
 
+import com.google.common.collect.Lists;
 import io.kaicode.elasticvc.api.BranchCriteria;
 import io.kaicode.elasticvc.api.BranchService;
 import io.kaicode.elasticvc.api.PathUtil;
 import io.kaicode.elasticvc.api.VersionControlHelper;
 import io.kaicode.elasticvc.domain.Branch;
+import io.kaicode.elasticvc.domain.Commit;
+import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap;
+import it.unimi.dsi.fastutil.longs.LongOpenHashSet;
 import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.search.sort.SortBuilders;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.snomed.snowstorm.config.SearchLanguagesConfiguration;
+import org.snomed.snowstorm.core.data.domain.Concepts;
 import org.snomed.snowstorm.core.data.domain.Description;
+import org.snomed.snowstorm.core.data.domain.Relationship;
 import org.snomed.snowstorm.core.util.DescriptionHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.data.elasticsearch.core.query.UpdateQuery;
 import org.springframework.data.elasticsearch.core.query.UpdateQueryBuilder;
@@ -24,10 +31,12 @@ import java.io.IOException;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 import static io.kaicode.elasticvc.api.ComponentService.LARGE_PAGE;
+import static java.lang.Long.parseLong;
 import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
-import static org.elasticsearch.index.query.QueryBuilders.termQuery;
+import static org.elasticsearch.index.query.QueryBuilders.*;
 
 @Service
 public class AdminOperationsService {
@@ -49,6 +58,9 @@ public class AdminOperationsService {
 
 	@Autowired
 	private DomainEntityConfiguration domainEntityConfiguration;
+
+	@Autowired
+	private ConceptUpdateHelper conceptUpdateHelper;
 
 	private Logger logger = LoggerFactory.getLogger(getClass());
 
@@ -133,5 +145,75 @@ public class AdminOperationsService {
 			throw new IllegalStateException(String.format("Branch %s at timepoint %s is already ended, it's not the latest commit.", branchPath, timepoint));
 		}
 		branchService.rollbackCompletedCommit(branchVersion, new ArrayList<>(domainEntityConfiguration.getAllDomainEntityTypes()));
+	}
+
+	public void restoreGroupNumberOfInactiveRelationships(String branchPath, String currentEffectiveTime, String previousReleaseBranch) {
+		logger.info("Restoring group number of inactive relationships on branch {}.", branchPath);
+
+		Map<Long, Relationship> inactiveRelationships = getAllInactiveRelationships(branchPath, currentEffectiveTime);
+		logger.info("{} relationships inactive on this branch branch with effectiveTime {}.", inactiveRelationships.size(), currentEffectiveTime);
+
+		BranchCriteria branchCriteria = versionControlHelper.getBranchCriteria(previousReleaseBranch);
+		NativeSearchQuery searchQuery = new NativeSearchQueryBuilder().withQuery(
+				boolQuery()
+						.must(branchCriteria.getEntityBranchCriteria(Relationship.class))
+						.must(termQuery(Relationship.Fields.ACTIVE, false))
+						.must(termQuery(Relationship.Fields.CHARACTERISTIC_TYPE_ID, Concepts.INFERRED_RELATIONSHIP)))
+				.withFilter(termsQuery(Relationship.Fields.RELATIONSHIP_ID, inactiveRelationships.keySet()))
+				.withPageable(LARGE_PAGE)
+				.build();
+
+		List<Relationship> correctedRelationships = new ArrayList<>();
+		try (CloseableIterator<Relationship> stream = elasticsearchTemplate.stream(searchQuery, Relationship.class)) {
+			stream.forEachRemaining(relationshipInPreviousRelease -> {
+				Relationship currentRelationship = inactiveRelationships.get(parseLong(relationshipInPreviousRelease.getRelationshipId()));
+				if (currentRelationship.getGroupId() != relationshipInPreviousRelease.getGroupId()) {
+					currentRelationship.setGroupId(relationshipInPreviousRelease.getGroupId());
+					currentRelationship.copyReleaseDetails(relationshipInPreviousRelease);
+					currentRelationship.markChanged();
+					correctedRelationships.add(currentRelationship);
+				}
+			});
+		}
+
+		List<String> first10Ids = correctedRelationships.subList(0, correctedRelationships.size() > 10 ? 10 : correctedRelationships.size())
+				.stream().map(Relationship::getRelationshipId).collect(Collectors.toList());
+
+		logger.info("{} relationships found on the previous release branch {} with a different role group, examples: {}",
+				correctedRelationships.size(), previousReleaseBranch, first10Ids);
+
+		if (!correctedRelationships.isEmpty()) {
+			try (Commit commit = branchService.openCommit(branchPath)) {
+				for (List<Relationship> batch : Lists.partition(correctedRelationships, 1_000)) {
+					logger.info("Correcting batch of {} relationships ...", batch.size());
+					conceptUpdateHelper.doSaveBatchRelationships(batch, commit);
+				}
+				commit.markSuccessful();
+			}
+			logger.info("All inactive relationship groups restored.");
+		}
+	}
+
+	private Map<Long, Relationship> getAllInactiveRelationships(String previousReleaseBranch, String effectiveTime) {
+		Map<Long, Relationship> relationshipMap = new Long2ObjectOpenHashMap<>();
+		try (CloseableIterator<Relationship> stream = elasticsearchTemplate.stream(new NativeSearchQueryBuilder()
+				.withQuery(
+						boolQuery()
+								.must(versionControlHelper.getBranchCriteria(previousReleaseBranch).getEntityBranchCriteria(Relationship.class))
+								.must(termQuery(Relationship.Fields.EFFECTIVE_TIME, effectiveTime))
+								.must(termQuery(Relationship.Fields.CHARACTERISTIC_TYPE_ID, Concepts.INFERRED_RELATIONSHIP))
+								.must(termQuery(Relationship.Fields.ACTIVE, false))
+						)
+				.withPageable(LARGE_PAGE)
+				.build(), Relationship.class)) {
+			stream.forEachRemaining(relationship -> {
+				relationshipMap.put(parseLong(relationship.getRelationshipId()), relationship);
+				if (relationshipMap.size() % 10_000 == 0) {
+					System.out.print(".");
+				}
+			});
+			System.out.println();
+		}
+		return relationshipMap;
 	}
 }
