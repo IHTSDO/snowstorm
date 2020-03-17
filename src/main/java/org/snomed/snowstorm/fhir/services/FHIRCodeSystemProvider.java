e11b6b2ac6692d22b5e11afab78288fdf65f5f95
Peter Groves Williams <petergwilliams@hotmail.com>
2020-03-17 10:22:01 +0000

    MAINT-1093 Add support for ValueSet $validate-code operation

--- src/main/java/org/snomed/snowstorm/fhir/services/FHIRCodeSystemProvider.java
+++ src/main/java/org/snomed/snowstorm/fhir/services/FHIRCodeSystemProvider.java
@@ -6,13 +6,11 @@ import ca.uhn.fhir.rest.server.IResourceProvider;
 
 import org.hl7.fhir.r4.model.*;
 import org.hl7.fhir.r4.model.OperationOutcome.IssueType;
-import org.apache.commons.lang.StringUtils;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.snomed.snowstorm.core.data.domain.Concept;
 import org.snomed.snowstorm.core.data.services.ConceptService;
 import org.snomed.snowstorm.core.data.services.MultiSearchService;
 import org.snomed.snowstorm.core.data.services.QueryService;
-import org.snomed.snowstorm.core.data.services.identifier.IdentifierService;
 import org.snomed.snowstorm.core.data.services.pojo.ConceptCriteria;
 import org.snomed.snowstorm.core.pojo.LanguageDialect;
 import org.snomed.snowstorm.fhir.config.FHIRConstants;
@@ -25,9 +23,7 @@ import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import static io.kaicode.elasticvc.api.ComponentService.LARGE_PAGE;
 
@@ -88,16 +84,16 @@ public class FHIRCodeSystemProvider implements IResourceProvider, FHIRConstants 
 			@OperationParam(name="date") DateTimeType date,
 			@OperationParam(name="coding") Coding coding,
 			@OperationParam(name="displayLanguage") String displayLanguage) throws FHIROperationException {
-		mutuallyExclusive("url", url, "codeSystem", codeSystem);
-		mutuallyExclusive("code", code, "coding", coding);
-		mutuallyRequired("display", display, "code", code, "coding", coding);
+		fhirHelper.mutuallyExclusive("url", url, "codeSystem", codeSystem);
+		fhirHelper.mutuallyExclusive("code", code, "coding", coding);
+		fhirHelper.mutuallyRequired("display", display, "code", code, "coding", coding);
 		if (coding != null && coding.getSystem() != null) {
-			mutuallyExclusive("version", version, "coding|codeSystem", coding.getSystem());
-			mutuallyExclusive("codeSystem", codeSystem, "coding|codeSystem", coding.getSystem());
+			fhirHelper.mutuallyExclusive("version", version, "coding|codeSystem", coding.getSystem());
+			fhirHelper.mutuallyExclusive("codeSystem", codeSystem, "coding|codeSystem", coding.getSystem());
 			codeSystem = new StringType (coding.getSystem());
 		} 
 		
-		notSupported("date", date);
+		fhirHelper.notSupported("date", date);
 		if (version != null) {
 			if (codeSystem == null) {
 				codeSystem = new StringType(SNOMED_URI + "/version/" + version.toString());
@@ -106,9 +102,8 @@ public class FHIRCodeSystemProvider implements IResourceProvider, FHIRConstants 
 			}
 		}
 		List<LanguageDialect> languageDialects = fhirHelper.getLanguageDialects(null, request);
-		String conceptId = recoverConceptId(code, coding);
+		String conceptId = fhirHelper.recoverConceptId(code, coding);
 		ConceptCriteria criteria = new ConceptCriteria().conceptIds(Collections.singleton(conceptId));
-		
 		Concept fullConcept = null;
 		if (codeSystem == null || codeSystem.toString().equals(SNOMED_URI)) {
 			Page<Concept> concepts = multiSearchService.findConcepts(criteria, PageRequest.of(0, 1));
@@ -126,48 +121,6 @@ public class FHIRCodeSystemProvider implements IResourceProvider, FHIRConstants 
 			return mapper.conceptNotFound();
 		} else {
 			return mapper.mapToFHIR(fullConcept, display);
-		}
-	}
-
-	private String recoverConceptId(CodeType code, Coding coding) throws FHIROperationException {
-		String conceptId = null;
-		if (code == null && coding == null) {
-			throw new FHIROperationException(IssueType.INVARIANT, "Use one of 'code' or 'coding' parameters");
-		} else if (code != null && coding == null) {
-			if (code.getCode().contains("|")) {
-				throw new FHIROperationException(IssueType.NOTSUPPORTED, "Please provide CodeSystem in codeSystem parameter");
-			}
-			conceptId = code.getCode();
-			if (!StringUtils.isNumeric(conceptId)) {
-				throw new FHIROperationException(IssueType.NOTSUPPORTED, "Only numeric SNOMED CT identifiers are currently supported");
-			}
-		} else if (code == null && coding != null) {
-			if (coding.getSystem() != null && !coding.getSystem().startsWith(SNOMED_URI)) {
-				throw new FHIROperationException(IssueType.NOTSUPPORTED, "CodeSystem of 'coding' must be based on" + SNOMED_URI);
-			}
-			conceptId = coding.getCode();
-		} else {
-			//Can only handle one of the two
-			throw new FHIROperationException(IssueType.INVARIANT, "Use either 'code' or 'coding' parameters, not both");
-		}
-		return conceptId;
-	}
-
-	private void mutuallyExclusive(String param1Name, Object param1, String param2Name, Object param2) throws FHIROperationException {
-		if (param1 != null && param2 != null) {
-			throw new FHIROperationException(IssueType.INVARIANT, "Use one of '" + param1Name + "' or '" + param2Name + "' parameters");
-		}
-	}
-
-	private void mutuallyRequired(String param1Name, Object param1, String param2Name, Object param2, String param3Name, Object param3) throws FHIROperationException {
-		if (param1 != null && param2 == null && param3 == null) {
-			throw new FHIROperationException(IssueType.INVARIANT, "Use of '" + param1Name + "' only allowed if '" + param2Name + "' or '" + param3Name + "' is also present");
-		}
-	}
-
-	private void notSupported(String paramName, Object obj) throws FHIROperationException {
-		if (obj != null) {
-			throw new FHIROperationException(IssueType.NOTSUPPORTED, "'" + paramName + "' parameter is not currently supported.");
 		}
 	}
 
