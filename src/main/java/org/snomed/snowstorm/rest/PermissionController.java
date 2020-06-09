92d285435e676b6e7acfe08ab069785315ef55b0
Kai Kewley <kaikewley@gmail.com>
2020-06-09 23:03:34 +0100

    MAINT-1338 Re-enable security sessions, use our new SSO library.

--- src/main/java/org/snomed/snowstorm/rest/PermissionController.java
+++ src/main/java/org/snomed/snowstorm/rest/PermissionController.java
@@ -6,7 +6,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.snomed.snowstorm.core.data.domain.security.PermissionRecord;
 import org.snomed.snowstorm.core.data.services.PermissionService;
-import org.snomed.snowstorm.rest.pojo.ItemsPage;
 import org.snomed.snowstorm.rest.pojo.UserGroupsPojo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
