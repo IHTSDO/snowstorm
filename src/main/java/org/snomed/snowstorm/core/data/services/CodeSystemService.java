964bd679883fa48ccf2183a03a49c20336a71e57
Kai Kewley <kaikewley@gmail.com>
2020-08-03 17:15:30 +0100

    MAINT-1413 Add API function to clear code system information cache.

--- src/main/java/org/snomed/snowstorm/core/data/services/CodeSystemService.java
+++ src/main/java/org/snomed/snowstorm/core/data/services/CodeSystemService.java
@@ -110,6 +110,10 @@ public class CodeSystemService {
 		}
 	}
 
+	public void clearCache() {
+		contentInformationCache.clear();
+	}
+
 	public boolean codeSystemExistsOnBranch(String branchPath) {
 		return findOneByBranchPath(branchPath) != null;
 	}
