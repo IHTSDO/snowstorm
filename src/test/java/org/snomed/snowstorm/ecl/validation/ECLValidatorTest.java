9a784574e0a2fdd104ef62fc180f7f722b73f5c4
Jesús Barros <jesus.barros.castro@gmail.com>
2020-08-20 21:50:32 +0200

    #145 Add existence validation to ECL validator

--- src/test/java/org/snomed/snowstorm/ecl/validation/ECLValidatorTest.java
+++ src/test/java/org/snomed/snowstorm/ecl/validation/ECLValidatorTest.java
@@ -8,12 +8,15 @@ import org.snomed.snowstorm.core.data.services.ConceptService;
 import org.snomed.snowstorm.core.data.services.ServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ECLValidatorTest extends AbstractTest {
 
-    private static final String MAIN_BRANCH = "MAIN";
+    private static final String BRANCH = "MAIN";
+
+    private static final String EXCEPTION_MESSAGE = "Concepts in the ECL request do not exist or are inactive on branch " + BRANCH + ": ";
 
     @Autowired
     private ConceptService conceptService;
@@ -22,19 +25,71 @@ class ECLValidatorTest extends AbstractTest {
     private ECLValidator eclValidator;
 
     @Test
-    public void validateEclExpressionWithActiveConcepts() throws ServiceException {
+    public void validateWithActiveConcepts() throws ServiceException {
         createActiveConcept("415360003");
 
         String eclExpression = "415360003 |GenSevere acute respiratory syndrome-related coronavirus (organism)|";
-        assertThatCode(() -> eclValidator.validateEcl(eclExpression, MAIN_BRANCH)).doesNotThrowAnyException();
+        assertThatCode(() -> eclValidator.validateEcl(eclExpression, BRANCH)).doesNotThrowAnyException();
     }
 
     @Test
-    public void failValidationEclExpressionWithInactiveConcepts() throws ServiceException {
+    public void validateWithActiveAndExistingConcepts() throws ServiceException {
+        createActiveConcept("246075003");
+        oneExistingConcept("387517004");
+
+        String eclExpression = "* : 246075003 |Causative agent| = 387517004 |Paracetamol|";
+        assertThatCode(() -> eclValidator.validateEcl(eclExpression, BRANCH)).doesNotThrowAnyException();
+    }
+
+    @Test
+    public void failValidationWithInactiveConcepts() throws ServiceException {
         createInactiveConcept("243608008");
 
         String eclExpression = "243608008 |Genus Coronavirus (organism)|";
-        assertThrows(IllegalArgumentException.class, () -> eclValidator.validateEcl(eclExpression, MAIN_BRANCH));
+        assertException(eclExpression, "243608008");
+    }
+
+    @Test
+    public void failValidationWithActiveAndNonexistentConcepts() throws ServiceException {
+        createActiveConcept("246075003");
+        nonexistentConcept("387517004");
+
+        String eclExpression = "* : 246075003 |Causative agent| = 387517004 |Paracetamol|";
+        assertException(eclExpression, "387517004");
+    }
+
+    @Test
+    public void failValidationWithInactiveAndExistingConcepts() throws ServiceException {
+        createInactiveConcept("246075003");
+        oneExistingConcept("387517004");
+
+        String eclExpression = "* : 246075003 |Causative agent| = 387517004 |Paracetamol|";
+        assertException(eclExpression, "246075003");
+    }
+
+    @Test
+    public void failValidationWithInactiveAndNonexistentConcepts() throws ServiceException {
+        createInactiveConcept("246075003");
+        nonexistentConcept("387517004");
+
+        String eclExpression = "* : 246075003 |Causative agent| = 387517004 |Paracetamol|";
+        assertException(eclExpression, "246075003", "387517004");
+    }
+
+    @Test
+    public void failValidationWithTwoInactiveAndOneNonexistentConcepts() throws ServiceException {
+        createInactiveConcept("105590001");
+        createInactiveConcept("127489000");
+        nonexistentConcept("249999999101");
+
+        String eclExpression = "< 105590001 |Substance|: R 127489000 |Has active ingredient| = 249999999101 |TRIPHASIL tablet|";
+        assertException(eclExpression, "105590001", "127489000", "249999999101");
+    }
+
+    private void assertException(String eclExpression, String... conceptIds) {
+        String exceptionMessage = EXCEPTION_MESSAGE + String.join(", ", conceptIds) + ".";
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> eclValidator.validateEcl(eclExpression, BRANCH));
+        assertThat(exception.getMessage()).isEqualTo(exceptionMessage);
     }
 
     private void createActiveConcept(String conceptId) throws ServiceException {
@@ -45,8 +100,18 @@ class ECLValidatorTest extends AbstractTest {
         createConcept(conceptId, false);
     }
 
+    private void oneExistingConcept(String conceptId) throws ServiceException {
+        createConcept(conceptId, true);
+    }
+
     private void createConcept(String conceptId, boolean isActive) throws ServiceException {
-        Concept inactiveConcept = new Concept(conceptId, 20020131, isActive, Concepts.CORE_MODULE, "900000000000074008");
-        conceptService.create(inactiveConcept, MAIN_BRANCH);
+        Concept concept = new Concept(conceptId, 20020131, isActive, Concepts.CORE_MODULE, "900000000000074008");
+        conceptService.create(concept, BRANCH);
+    }
+
+    private void nonexistentConcept(String conceptId) {
+        if (conceptService.find(conceptId, BRANCH) != null) {
+            conceptService.deleteConceptAndComponents(conceptId, BRANCH, true);
+        }
     }
 }
