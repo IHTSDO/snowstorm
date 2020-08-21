b9eaf4610c3ea3741b63e1748c1e76f03804bc06
Jesús Barros <jesus.barros.castro@gmail.com>
2020-08-21 07:45:33 +0200

    #145 Fix minor format issues

--- src/test/java/org/snomed/snowstorm/ecl/domain/SRefinementTest.java
+++ src/test/java/org/snomed/snowstorm/ecl/domain/SRefinementTest.java
@@ -44,12 +44,12 @@ class SRefinementTest {
         String eclExpression =
                 "< 404684003 |Clinical finding|:" +
                         "{" +
-                        "363698007 |Finding site| = << 39057004 |Pulmonary valve structure|," +
-                        "116676008 |Associated morphology| = << 415582006 |Stenosis|" +
-                        "}, " +
+                            "363698007 |Finding site| = << 39057004 |Pulmonary valve structure|," +
+                            "116676008 |Associated morphology| = << 415582006 |Stenosis|" +
+                        "}," +
                         "{" +
-                        "363698007 |Finding site| = << 53085002 |Right ventricular structure|," +
-                        "116676008 |Associated morphology| = << 56246009 |Hypertrophy|" +
+                            "363698007 |Finding site| = << 53085002 |Right ventricular structure|," +
+                            "116676008 |Associated morphology| = << 56246009 |Hypertrophy|" +
                         "}";
 
         Set<String> actualConceptIds = getConceptIdsScenario(eclExpression);
