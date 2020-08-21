9408f587d4ea851fe883188e926c7882f88c214f
Jesús Barros <jesus.barros.castro@gmail.com>
2020-08-21 23:23:54 +0200

    #145 Improve test coverage by adding new test cases

--- src/test/java/org/snomed/snowstorm/ecl/domain/SRefinementTest.java
+++ src/test/java/org/snomed/snowstorm/ecl/domain/SRefinementTest.java
@@ -69,6 +69,69 @@ class SRefinementTest {
     }
 
     @Test
+    public void getConceptIdsInDottedExpressionConstraint() {
+        String eclExpression = "< 125605004 |Fracture of bone| . 363698007 |Finding site|";
+
+        Set<String> actualConceptIds = getConceptIdsScenario(eclExpression);
+
+        Set<String> expectedConceptIds = newHashSet("125605004", "363698007");
+        assertThat(actualConceptIds).containsExactlyInAnyOrderElementsOf(expectedConceptIds);
+    }
+
+    @Test
+    public void getConceptIdsInExpressionWithAttributeConjunction() {
+        String eclExpression =
+                "< 404684003 |Clinical finding|:" +
+                        "363698007 |Finding site| =<< 39057004 |Pulmonary valve structure|" +
+                            "AND " +
+                        "116676008 |Associated morphology| =<< 415582006 |Stenosis|";
+
+        Set<String> actualConceptIds = getConceptIdsScenario(eclExpression);
+
+        Set<String> expectedConceptIds = newHashSet("404684003", "363698007", "39057004", "116676008", "415582006");
+        assertThat(actualConceptIds).containsExactlyInAnyOrderElementsOf(expectedConceptIds);
+    }
+
+    @Test
+    public void getConceptIdsInExpressionWithAttributeDisjunction() {
+        String eclExpression =
+                "< 404684003 |Clinical finding|:" +
+                        "116676008 |Associated morphology| =<< 55641003 |Infarct|" +
+                            " OR " +
+                        "42752001 |Due to|=<< 22298006 |Myocardial infarction|";
+
+        Set<String> actualConceptIds = getConceptIdsScenario(eclExpression);
+
+        Set<String> expectedConceptIds = newHashSet("404684003", "116676008", "55641003", "42752001", "22298006");
+        assertThat(actualConceptIds).containsExactlyInAnyOrderElementsOf(expectedConceptIds);
+    }
+
+    @Test
+    public void getConceptIdsInExpressionWithSubAttributeSet() {
+        String eclExpression =
+                "404684003 |Clinical finding|: " +
+                    "(" +
+                        "363698007 |Finding site| = 39057004 |Pulmonary valve structure|," +
+                        "363698007 |Finding site| = 299701004 |Bone of forearm|" +
+                    ")";
+
+        Set<String> actualConceptIds = getConceptIdsScenario(eclExpression);
+
+        Set<String> expectedConceptIds = newHashSet("404684003", "363698007", "39057004", "299701004");
+        assertThat(actualConceptIds).containsExactlyInAnyOrderElementsOf(expectedConceptIds);
+    }
+
+    @Test
+    public void getConceptIdsInExpressionWithSubRefinement() {
+        String eclExpression ="< 404684003 |Clinical finding|: ({363698007 |finding site| = 20760004 |shaft of humerus|, 116676008 |associated morphology| = 73737008 |Fracture, spiral| })";
+
+        Set<String> actualConceptIds = getConceptIdsScenario(eclExpression);
+
+        Set<String> expectedConceptIds = newHashSet("404684003", "363698007", "20760004", "116676008", "73737008");
+        assertThat(actualConceptIds).containsExactlyInAnyOrderElementsOf(expectedConceptIds);
+    }
+
+    @Test
     public void getConceptIdsInComplexCardinalityExpressionConstraint() {
         String eclExpression = "< 404684003 |Clinical finding|: [0..0] {[2..*] 363698007 |Finding site| =< 91723000 |Anatomical structure|}";
 
@@ -80,7 +143,7 @@ class SRefinementTest {
 
     @Test
     public void getConceptIdsInConjunctionExpressionConstraint() {
-        String eclExpression = "< 19829001 |Disorder of lung| AND  < 301867009 |Edema of trunk|";
+        String eclExpression = "< 19829001 |Disorder of lung| AND < 301867009 |Edema of trunk|";
 
         Set<String> actualConceptIds = getConceptIdsScenario(eclExpression);
 
@@ -90,11 +153,30 @@ class SRefinementTest {
 
     @Test
     public void getConceptIdsInDisjunctionExpressionConstraint() {
-        String eclExpression = "< 19829001 |Disorder of lung| OR  < 301867009 |Edema of trunk|";
+        String eclExpression = "< 19829001 |Disorder of lung| OR < 301867009 |Edema of trunk|";
 
         Set<String> actualConceptIds = getConceptIdsScenario(eclExpression);
 
         Set<String> expectedConceptIds = newHashSet("19829001", "301867009");
+        assertThat(actualConceptIds).containsExactlyInAnyOrderElementsOf(expectedConceptIds);
+    }
+
+    @Test
+    public void getConceptIdsInComplexDisjunctionExpressionConstraint() {
+        String eclExpression = "< 404684003 |Clinical finding|:" +
+                "{" +
+                    "363698007 |Finding site| =<< 39057004 |Pulmonary valve structure|," +
+                    "116676008 |Associated morphology| =<< 415582006 |Stenosis|" +
+                "}" +
+                    " OR " +
+                "{" +
+                    "363698007 |Finding site| =<< 53085002 |Right ventricular structure|," +
+                    "116676008 |Associated morphology| =<< 56246009 |Hypertrophy|" +
+                "}";
+
+        Set<String> actualConceptIds = getConceptIdsScenario(eclExpression);
+
+        Set<String> expectedConceptIds = newHashSet("404684003", "363698007", "39057004", "116676008", "415582006", "53085002", "56246009");
         assertThat(actualConceptIds).containsExactlyInAnyOrderElementsOf(expectedConceptIds);
     }
 
@@ -110,7 +192,7 @@ class SRefinementTest {
 
     @Test
     public void getConceptIdsInNotEqualsExpressionConstraint() {
-        String eclExpression = "<  404684003 |Clinical finding|: [0..0] 116676008 |Associated morphology| = << 26036001 |Obstruction|";
+        String eclExpression = "< 404684003 |Clinical finding|: [0..0] 116676008 |Associated morphology| = << 26036001 |Obstruction|";
 
         Set<String> actualConceptIds = getConceptIdsScenario(eclExpression);
 
