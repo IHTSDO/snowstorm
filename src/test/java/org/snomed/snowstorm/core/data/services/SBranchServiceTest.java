87d2d1b0bdfb68b438eb9fdbc9120756733e95a7
CoderMChu <mch@ihtsdo.org>
2020-09-14 11:57:54 +0100

    MAINT-1445 Updated tests to Junit5 in SBranchServiceTest class

--- src/test/java/org/snomed/snowstorm/core/data/services/SBranchServiceTest.java
+++ src/test/java/org/snomed/snowstorm/core/data/services/SBranchServiceTest.java
@@ -2,21 +2,17 @@ package org.snomed.snowstorm.core.data.services;
 
 import io.kaicode.elasticvc.api.BranchService;
 import io.kaicode.elasticvc.domain.Branch;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.snomed.snowstorm.AbstractTest;
-import org.snomed.snowstorm.TestConfig;
 import org.snomed.snowstorm.core.data.domain.Concept;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = TestConfig.class)
-public class SBranchServiceTest extends AbstractTest {
+class SBranchServiceTest extends AbstractTest {
 
 	@Autowired
 	private ConceptService conceptService;
@@ -48,13 +44,12 @@ public class SBranchServiceTest extends AbstractTest {
 		assertNull("Expect null because there is no partial commit on the branch.", sBranchService.getPartialCommitTimestamp(branch));
 	}
 
-	@Test(expected = IllegalStateException.class)
+	@Test
 	public void rollbackPartialCommit() throws ServiceException {
 		String branch = "MAIN";
 		String conceptId = "100001";
 		conceptService.create(new Concept(conceptId, "10000111"), branch);
-		sBranchService.rollbackPartialCommit(branch);
-		// Throws illegal state because there is no partial commit on the branch
+		Assertions.assertThrows(IllegalStateException.class, () -> sBranchService.rollbackPartialCommit(branch),
+				"Throws illegal state because there is no partial commit on the branch");
 	}
-
 }
