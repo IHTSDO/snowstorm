46c3398a4c5bd657dc4beeb6e5ed513ea3031e4d
Kai Kewley <kaikewley@gmail.com>
2020-09-23 10:47:10 +0100

    MAINT-1455 New API utility function: parse ECL, convert to JSON model.

--- src/main/java/org/snomed/snowstorm/ecl/domain/SRefinement.java
+++ src/main/java/org/snomed/snowstorm/ecl/domain/SRefinement.java
@@ -1,9 +1,12 @@
 package org.snomed.snowstorm.ecl.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import java.util.Set;
 
 public interface SRefinement {
 	void addCriteria(RefinementBuilder refinementBuilder);
 
+	@JsonIgnore
 	Set<String> getConceptIds();
 }
