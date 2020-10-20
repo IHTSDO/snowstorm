7f6bd19d24ed6ab783b1074866104c8a3a1d530a
Kai Kewley <kaikewley@gmail.com>
2020-10-20 15:18:22 +0100

    MRCMMT-253 New API to fetch concept model attribute hierarchy.

--- src/main/java/org/snomed/snowstorm/mrcm/MRCMService.java
+++ src/main/java/org/snomed/snowstorm/mrcm/MRCMService.java
@@ -1,5 +1,6 @@
 package org.snomed.snowstorm.mrcm;
 
+import ch.qos.logback.classic.Level;
 import io.kaicode.elasticvc.api.BranchCriteria;
 import io.kaicode.elasticvc.api.VersionControlHelper;
 import org.slf4j.Logger;
@@ -7,27 +8,37 @@ import org.slf4j.LoggerFactory;
 import org.snomed.langauges.ecl.domain.refinement.Operator;
 import org.snomed.snowstorm.core.data.domain.ConceptMini;
 import org.snomed.snowstorm.core.data.domain.Concepts;
+import org.snomed.snowstorm.core.data.domain.QueryConcept;
 import org.snomed.snowstorm.core.data.domain.Relationship;
 import org.snomed.snowstorm.core.data.services.ConceptService;
 import org.snomed.snowstorm.core.data.services.QueryService;
 import org.snomed.snowstorm.core.data.services.ServiceException;
 import org.snomed.snowstorm.core.data.services.identifier.IdentifierService;
 import org.snomed.snowstorm.core.pojo.LanguageDialect;
+import org.snomed.snowstorm.core.util.TimerUtil;
 import org.snomed.snowstorm.mrcm.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.core.SearchHitsIterator;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import static io.kaicode.elasticvc.api.VersionControlHelper.LARGE_PAGE;
 import static java.lang.Long.parseLong;
+import static org.elasticsearch.index.query.QueryBuilders.*;
 
 @Service
 public class MRCMService {
 
 	private static final PageRequest RESPONSE_PAGE_SIZE = PageRequest.of(0, 50);
+	private static final String CHILDREN = "children";
 
 	@Autowired
 	private MRCMLoader mrcmLoader;
@@ -41,7 +52,10 @@ public class MRCMService {
 	@Autowired
 	private VersionControlHelper versionControlHelper;
 
-	private Logger logger = LoggerFactory.getLogger(getClass());
+	@Autowired
+	private ElasticsearchRestTemplate elasticsearchTemplate;
+
+	private final Logger logger = LoggerFactory.getLogger(getClass());
 
 	// Hardcoded Is a (attribute)
 	// 'Is a' is not really an attribute at all but it's convenient for implementations to have this.
@@ -142,6 +156,55 @@ public class MRCMService {
 		}
 
 		return queryService.search(conceptQuery, branchPath, RESPONSE_PAGE_SIZE).getContent();
+	}
+
+	public ConceptMini retrieveConceptModelAttributeHierarchy(String branch, List<LanguageDialect> languageDialects) {
+		logger.info("Loading concept model attribute hierarchy.");
+		TimerUtil timer = new TimerUtil("attribute-tree", Level.INFO);
+		String topId = Concepts.CONCEPT_MODEL_ATTRIBUTE;
+		long topIdLong = parseLong(topId);
+
+		// Load all attributes including terms
+		List<ConceptMini> allAttributes = ecl("<<" + topId, branch, languageDialects);
+		timer.checkpoint("load all with terms");
+		Map<Long, ConceptMini> attributeMap = allAttributes.stream().collect(Collectors.toMap(ConceptMini::getConceptIdAsLong, Function.identity()));
+		if (!attributeMap.containsKey(topIdLong)) {
+			throw new IllegalStateException("Concept not found: " + topId + " | Concept model attribute (attribute) |.");
+		}
+		Set<Long> remainingAttributes = new HashSet<>(attributeMap.keySet());
+		remainingAttributes.remove(topIdLong);
+
+		BranchCriteria branchCriteria = versionControlHelper.getBranchCriteria(branch);
+
+		NativeSearchQueryBuilder queryConceptQuery = new NativeSearchQueryBuilder()
+				.withQuery(boolQuery()
+						.must(branchCriteria.getEntityBranchCriteria(QueryConcept.class))
+						.must(termQuery(QueryConcept.Fields.STATED, false))
+						.filter(termsQuery(QueryConcept.Fields.CONCEPT_ID, remainingAttributes))
+				)
+				.withFields(QueryConcept.Fields.CONCEPT_ID, QueryConcept.Fields.PARENTS)
+				.withPageable(LARGE_PAGE);
+		try (SearchHitsIterator<QueryConcept> queryConcepts = elasticsearchTemplate.searchForStream(queryConceptQuery.build(), QueryConcept.class)) {
+			queryConcepts.forEachRemaining(hit -> {
+				for (Long parent : hit.getContent().getParents()) {
+					ConceptMini parentMini = attributeMap.get(parent);
+					if (parentMini.getExtraFields() == null || parentMini.getExtraFields().get(CHILDREN) == null) {
+						parentMini.addExtraField(CHILDREN, new ArrayList<>());
+					}
+					@SuppressWarnings("unchecked")
+					List<ConceptMini> children = (List<ConceptMini>) parentMini.getExtraFields().get(CHILDREN);
+					children.add(attributeMap.get(hit.getContent().getConceptIdL()));
+					children.sort(Comparator.comparing(ConceptMini::getFsnTerm));
+				}
+			});
+		}
+		timer.finish();
+
+		return attributeMap.get(topIdLong);
+	}
+
+	private List<ConceptMini> ecl(String ecl, String branch, List<LanguageDialect> languageDialects) {
+		return queryService.search(queryService.createQueryBuilder(false).resultLanguageDialects(languageDialects).ecl(ecl), branch, PageRequest.of(0, 1_000)).getContent();
 	}
 
 }
