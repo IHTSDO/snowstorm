e0f69515f13d1591f1accc5db65c62f3c34d0abc
dmcgihtsdo <dmcg@ihtsdo.org>
2020-12-09 17:42:27 +0000

    CDI-90 Add caching to MRCMLoader

--- src/main/java/org/snomed/snowstorm/mrcm/MRCMLoader.java
+++ src/main/java/org/snomed/snowstorm/mrcm/MRCMLoader.java
@@ -18,116 +18,149 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static io.kaicode.elasticvc.api.ComponentService.LARGE_PAGE;
 
 @Service
 public class MRCMLoader {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MRCMLoader.class);
 
-	@Autowired
-	private ECLQueryBuilder eclQueryBuilder;
+    private final Map<String, MRCM> cache = new HashMap<>();
 
-	@Autowired
-	private ReferenceSetMemberService memberService;
+    @Autowired
+    private ECLQueryBuilder eclQueryBuilder;
 
-	private static Logger logger = LoggerFactory.getLogger(MRCMLoader.class);
+    @Autowired
+    private ReferenceSetMemberService memberService;
 
-	// TODO: Make this work for MRCM extensions. Ask Guillermo how he is extending the MRCM in Extensions TermMed are maintaining.
-	MRCM loadActiveMRCM(String branchPath, BranchCriteria branchCriteria) throws ServiceException {
-		TimerUtil timer = new TimerUtil("MRCM");
+    // TODO: Make this work for MRCM extensions. Ask Guillermo how he is extending the MRCM in Extensions TermMed are maintaining.
+    public MRCM loadActiveMRCM(String branchPath, BranchCriteria branchCriteria) throws ServiceException {
+        LOGGER.debug("Checking cache for MRCM.");
+        final MRCM cachedMRCM = cache.get(branchPath);
+        if (cachedMRCM != null) {
+            LOGGER.debug("MRCM present in cache.");
+            return cachedMRCM;
+        }
+        LOGGER.debug("MRCM not present in cache; loading MRCM.");
 
-		List<ReferenceSetMember> domainMembers = memberService.findMembers(branchPath, branchCriteria,
-				new MemberSearchRequest().referenceSet(Concepts.REFSET_MRCM_DOMAIN_INTERNATIONAL), LARGE_PAGE).getContent();
+        final TimerUtil timer = new TimerUtil("MRCM");
+        final List<Domain> domains = getDomains(branchPath, branchCriteria, timer);
+        final List<AttributeDomain> attributeDomains = getAttributeDomains(branchPath, branchCriteria, timer);
+        final List<AttributeRange> attributeRanges = getAttributeRanges(branchPath, branchCriteria, timer);
+        final MRCM mrcm = new MRCM(domains, attributeDomains, attributeRanges);
 
-		List<Domain> domains = new ArrayList<>();
-		for (ReferenceSetMember member : domainMembers) {
-			// id	effectiveTime	active	moduleId	refsetId	referencedComponentId	domainConstraint	parentDomain
-			// proximalPrimitiveConstraint	proximalPrimitiveRefinement	domainTemplateForPrecoordination	domainTemplateForPostcoordination	guideURL
-			if (member.isActive()) {
-				domains.add(new Domain(
-						member.getMemberId(),
-						member.getEffectiveTime(),
-						member.isActive(),
-						member.getReferencedComponentId(),
-						getConstraint(member, "domainConstraint"),
-						member.getAdditionalField("parentDomain"),
-						getConstraint(member, "proximalPrimitiveConstraint"),
-						member.getAdditionalField("proximalPrimitiveRefinement"),
-						member.getAdditionalField("domainTemplateForPrecoordination"),
-						member.getAdditionalField("domainTemplateForPostcoordination")
-				));
-			}
-		}
-		timer.checkpoint("Load domains");
+        cache.putIfAbsent(branchPath, mrcm);
+        return mrcm;
+    }
 
-		List<ReferenceSetMember> attributeDomainMembers = memberService.findMembers(branchPath, branchCriteria,
-				new MemberSearchRequest().referenceSet(Concepts.REFSET_MRCM_ATTRIBUTE_DOMAIN_INTERNATIONAL), LARGE_PAGE).getContent();
+    private List<Domain> getDomains(final String branchPath,
+                                    final BranchCriteria branchCriteria,
+                                    final TimerUtil timer) {
+        List<ReferenceSetMember> domainMembers = memberService.findMembers(branchPath, branchCriteria,
+                new MemberSearchRequest().referenceSet(Concepts.REFSET_MRCM_DOMAIN_INTERNATIONAL), LARGE_PAGE).getContent();
 
-		List<AttributeDomain> attributeDomains = new ArrayList<>();
-		for (ReferenceSetMember member : attributeDomainMembers) {
-			// id	effectiveTime	active	moduleId	refsetId	referencedComponentId	domainId
-			// grouped	attributeCardinality	attributeInGroupCardinality	ruleStrengthId	contentTypeId
-			if (member.isActive()) {
-				attributeDomains.add(new AttributeDomain(
-						member.getMemberId(),
-						member.getEffectiveTime(),
-						member.isActive(),
-						member.getReferencedComponentId(),
-						member.getAdditionalField("domainId"),
-						"1".equals(member.getAdditionalField("grouped")),
-						new Cardinality(member.getAdditionalField("attributeCardinality")),
-						new Cardinality(member.getAdditionalField("attributeInGroupCardinality")),
-						RuleStrength.lookupByConceptId(member.getAdditionalField("ruleStrengthId")),
-						ContentType.lookupByConceptId(member.getAdditionalField("contentTypeId"))
-				));
-			}
-		}
-		timer.checkpoint("Load attribute domains");
+        List<Domain> domains = new ArrayList<>();
+        for (ReferenceSetMember member : domainMembers) {
+            // id	effectiveTime	active	moduleId	refsetId	referencedComponentId	domainConstraint	parentDomain
+            // proximalPrimitiveConstraint	proximalPrimitiveRefinement	domainTemplateForPrecoordination	domainTemplateForPostcoordination	guideURL
+            if (member.isActive()) {
+                domains.add(new Domain(
+                        member.getMemberId(),
+                        member.getEffectiveTime(),
+                        member.isActive(),
+                        member.getReferencedComponentId(),
+                        getConstraint(member, "domainConstraint"),
+                        member.getAdditionalField("parentDomain"),
+                        getConstraint(member, "proximalPrimitiveConstraint"),
+                        member.getAdditionalField("proximalPrimitiveRefinement"),
+                        member.getAdditionalField("domainTemplateForPrecoordination"),
+                        member.getAdditionalField("domainTemplateForPostcoordination")
+                ));
+            }
+        }
+        timer.checkpoint("Load domains");
 
+        return domains;
+    }
 
-		List<ReferenceSetMember> attributeRangeMembers = memberService.findMembers(branchPath, branchCriteria,
-				new MemberSearchRequest().referenceSet(Concepts.REFSET_MRCM_ATTRIBUTE_RANGE_INTERNATIONAL), LARGE_PAGE).getContent();
+    private List<AttributeDomain> getAttributeDomains(final String branchPath,
+                                                      final BranchCriteria branchCriteria,
+                                                      final TimerUtil timer) throws ServiceException {
+        List<ReferenceSetMember> attributeDomainMembers = memberService.findMembers(branchPath, branchCriteria,
+                new MemberSearchRequest().referenceSet(Concepts.REFSET_MRCM_ATTRIBUTE_DOMAIN_INTERNATIONAL), LARGE_PAGE).getContent();
 
-		List<AttributeRange> attributeRanges = new ArrayList<>();
-		for (ReferenceSetMember member : attributeRangeMembers) {
-			// id	effectiveTime	active	moduleId	refsetId	referencedComponentId	rangeConstraint	attributeRule	ruleStrengthId	contentTypeId
-			if (member.isActive()) {
-				attributeRanges.add(new AttributeRange(
-						member.getMemberId(),
-						member.getEffectiveTime(),
-						member.isActive(),
-						member.getReferencedComponentId(),
-						member.getAdditionalField("rangeConstraint"),
-						member.getAdditionalField("attributeRule"),
-						RuleStrength.lookupByConceptId(member.getAdditionalField("ruleStrengthId")),
-						ContentType.lookupByConceptId(member.getAdditionalField("contentTypeId"))
-				));
-			}
-		}
-		timer.checkpoint("Load attribute ranges");
+        List<AttributeDomain> attributeDomains = new ArrayList<>();
+        for (ReferenceSetMember member : attributeDomainMembers) {
+            // id	effectiveTime	active	moduleId	refsetId	referencedComponentId	domainId
+            // grouped	attributeCardinality	attributeInGroupCardinality	ruleStrengthId	contentTypeId
+            if (member.isActive()) {
+                attributeDomains.add(new AttributeDomain(
+                        member.getMemberId(),
+                        member.getEffectiveTime(),
+                        member.isActive(),
+                        member.getReferencedComponentId(),
+                        member.getAdditionalField("domainId"),
+                        "1".equals(member.getAdditionalField("grouped")),
+                        new Cardinality(member.getAdditionalField("attributeCardinality")),
+                        new Cardinality(member.getAdditionalField("attributeInGroupCardinality")),
+                        RuleStrength.lookupByConceptId(member.getAdditionalField("ruleStrengthId")),
+                        ContentType.lookupByConceptId(member.getAdditionalField("contentTypeId"))
+                ));
+            }
+        }
+        timer.checkpoint("Load attribute domains");
 
-		return new MRCM(domains, attributeDomains, attributeRanges);
-	}
+        return attributeDomains;
+    }
 
-	private Constraint getConstraint(ReferenceSetMember mrcmMember, String fieldName) {
-		String constraint = mrcmMember.getAdditionalField(fieldName);
-		if (constraint == null || constraint.isEmpty()) {
-			logger.warn("No constraint found for '{}' in member {}.", fieldName, mrcmMember.getMemberId());
-			return null;
-		}
-		ExpressionConstraint ecl = eclQueryBuilder.createQuery(constraint);
-		if (ecl instanceof SubExpressionConstraint) {
-			SubExpressionConstraint sub = (SubExpressionConstraint) ecl;
-			return new Constraint(constraint, sub.getConceptId(), sub.getOperator());
-		} else if (ecl instanceof RefinedExpressionConstraint) {
-			RefinedExpressionConstraint refined = (RefinedExpressionConstraint) ecl;
-			SubExpressionConstraint sub = refined.getSubexpressionConstraint();
-			return new Constraint(constraint, sub.getConceptId(), sub.getOperator());
-		} else {
-			logger.error("Unable to process MRCM constraint '{}' in member {}.", constraint, mrcmMember.getMemberId());
-		}
-		return null;
-	}
+    private List<AttributeRange> getAttributeRanges(final String branchPath,
+                                                    final BranchCriteria branchCriteria,
+                                                    final TimerUtil timer) {
+        List<ReferenceSetMember> attributeRangeMembers = memberService.findMembers(branchPath, branchCriteria,
+                new MemberSearchRequest().referenceSet(Concepts.REFSET_MRCM_ATTRIBUTE_RANGE_INTERNATIONAL), LARGE_PAGE).getContent();
+
+        List<AttributeRange> attributeRanges = new ArrayList<>();
+        for (ReferenceSetMember member : attributeRangeMembers) {
+            // id	effectiveTime	active	moduleId	refsetId	referencedComponentId	rangeConstraint	attributeRule	ruleStrengthId	contentTypeId
+            if (member.isActive()) {
+                attributeRanges.add(new AttributeRange(
+                        member.getMemberId(),
+                        member.getEffectiveTime(),
+                        member.isActive(),
+                        member.getReferencedComponentId(),
+                        member.getAdditionalField("rangeConstraint"),
+                        member.getAdditionalField("attributeRule"),
+                        RuleStrength.lookupByConceptId(member.getAdditionalField("ruleStrengthId")),
+                        ContentType.lookupByConceptId(member.getAdditionalField("contentTypeId"))
+                ));
+            }
+        }
+        timer.checkpoint("Load attribute ranges");
+
+        return attributeRanges;
+    }
+
+    private Constraint getConstraint(ReferenceSetMember mrcmMember, String fieldName) {
+        String constraint = mrcmMember.getAdditionalField(fieldName);
+        if (constraint == null || constraint.isEmpty()) {
+            LOGGER.warn("No constraint found for '{}' in member {}.", fieldName, mrcmMember.getMemberId());
+            return null;
+        }
+        ExpressionConstraint ecl = eclQueryBuilder.createQuery(constraint);
+        if (ecl instanceof SubExpressionConstraint) {
+            SubExpressionConstraint sub = (SubExpressionConstraint) ecl;
+            return new Constraint(constraint, sub.getConceptId(), sub.getOperator());
+        } else if (ecl instanceof RefinedExpressionConstraint) {
+            RefinedExpressionConstraint refined = (RefinedExpressionConstraint) ecl;
+            SubExpressionConstraint sub = refined.getSubexpressionConstraint();
+            return new Constraint(constraint, sub.getConceptId(), sub.getOperator());
+        } else {
+            LOGGER.error("Unable to process MRCM constraint '{}' in member {}.", constraint, mrcmMember.getMemberId());
+        }
+        return null;
+    }
 
 }
