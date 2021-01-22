5c4a26e2a8c5b4d15d26e625a9fcba8f6a6dc862
Quyen Ly <quocquyenit@gmail.com>
2021-01-22 10:32:20 +0700

    CDI-45 Updated unit tests

--- src/test/java/org/snomed/snowstorm/validation/DroolValidationServiceTest.java
+++ src/test/java/org/snomed/snowstorm/validation/DroolValidationServiceTest.java
@@ -1,18 +1,11 @@
 package org.snomed.snowstorm.validation;
 
 import io.kaicode.elasticvc.api.BranchService;
-import org.apache.commons.io.FileUtils;
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.lib.TextProgressMonitor;
 import org.ihtsdo.drools.response.InvalidContent;
 import org.ihtsdo.drools.response.Severity;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.snomed.snowstorm.AbstractTest;
 import org.snomed.snowstorm.TestConfig;
 import org.snomed.snowstorm.core.data.domain.*;
@@ -20,29 +13,20 @@ import org.snomed.snowstorm.core.data.services.BranchMetadataKeys;
 import org.snomed.snowstorm.core.data.services.ConceptService;
 import org.snomed.snowstorm.core.data.services.ServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 import static org.snomed.snowstorm.core.data.domain.Concepts.*;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = TestConfig.class)
 public class DroolValidationServiceTest extends AbstractTest {
-    private final Logger logger = LoggerFactory.getLogger(getClass());
-
-    private static boolean checkoutRulesDone = false;
-
     private static final String DEFAULT_BRANCH = "MAIN";
 
     @Autowired
@@ -54,25 +38,20 @@ public class DroolValidationServiceTest extends AbstractTest {
     @Autowired
     private BranchService branchService;
 
-    @Value("${validation.drools.rules.path}")
-    private String droolsRulesPath;
-
     @BeforeEach
-    void setup() throws ServiceException, GitAPIException, IOException {
-        checkoutDroolsRules();
-
+    void setup() throws ServiceException {
         Map <String, String> metadata = new HashMap <>();
         metadata.put(BranchMetadataKeys.ASSERTION_GROUP_NAMES, "common-authoring");
         branchService.updateMetadata(DEFAULT_BRANCH, metadata);
 
         conceptService.create(new Concept(SNOMEDCT_ROOT, null, true, CORE_MODULE, PRIMITIVE), DEFAULT_BRANCH);
         conceptService.create(new Concept(ISA, null, true, CORE_MODULE, Concepts.PRIMITIVE), DEFAULT_BRANCH);
+        conceptService.create(new Concept("23131313", null, true, CORE_MODULE, Concepts.PRIMITIVE), DEFAULT_BRANCH);
 
         String conceptId = "100001";
         Concept concept = new Concept(conceptId, null, true, CORE_MODULE, PRIMITIVE);
         concept.addDescription(new Description("12220000", null, true, CORE_MODULE, conceptId, "en", FSN, "Test (event)", Concepts.CASE_INSENSITIVE));
         concept.addDescription(new Description("12220003", null, true, CORE_MODULE, conceptId, "en", SYNONYM, "Test", CASE_INSENSITIVE));
-
         conceptService.create(concept, DEFAULT_BRANCH);
     }
 
@@ -84,10 +63,11 @@ public class DroolValidationServiceTest extends AbstractTest {
         foundConcept.getDescriptions().stream().forEach(description -> description.setAcceptabilityMap(Collections.singletonMap(Concepts.US_EN_LANG_REFSET,
                 Concepts.descriptionAcceptabilityNames.get(PREFERRED))));
 
-        foundConcept.addRelationship(new Relationship("100002", ISA, SNOMEDCT_ROOT).setCharacteristicTypeId(Concepts.STATED_RELATIONSHIP));
+        foundConcept.addRelationship(new Relationship("100002", ISA, SNOMEDCT_ROOT));
+        foundConcept.addAxiom(new Relationship("100002", ISA, SNOMEDCT_ROOT));
 
         final Concept updatedConcept = conceptService.update(foundConcept, DEFAULT_BRANCH);
-        assertEquals(1, updatedConcept.getRelationships().size());
+        assertEquals(1, updatedConcept.getClassAxioms().size());
         List <InvalidContent> invalidContents = droolValidationService.validateConcept(DEFAULT_BRANCH, updatedConcept);
         assertEquals(3, invalidContents.size());
 
@@ -97,11 +77,11 @@ public class DroolValidationServiceTest extends AbstractTest {
 
         index++;
         assertEquals(Severity.WARNING, invalidContents.get(index).getSeverity());
-        assertEquals("Active FSN should end with a valid semantic tag.", invalidContents.get(index).getMessage());
+        assertEquals("A concept's semantic tags should be compatible with those of the active parents.", invalidContents.get(index).getMessage());
 
         index++;
         assertEquals(Severity.WARNING, invalidContents.get(index).getSeverity());
-        assertEquals("A concept's semantic tags should be compatible with those of the active parents.", invalidContents.get(index).getMessage());
+        assertEquals("Active FSN should end with a valid semantic tag.", invalidContents.get(index).getMessage());
     }
 
     @Test
@@ -113,7 +93,7 @@ public class DroolValidationServiceTest extends AbstractTest {
 
         final Concept updatedConcept = conceptService.update(foundConcept, DEFAULT_BRANCH);
 
-        assertEquals(0, updatedConcept.getRelationships().size());
+        assertEquals(0, updatedConcept.getClassAxioms().size());
         List <InvalidContent> invalidContents = droolValidationService.validateConcept(DEFAULT_BRANCH, updatedConcept);
         assertEquals(4, invalidContents.size());
 
@@ -122,16 +102,16 @@ public class DroolValidationServiceTest extends AbstractTest {
         assertEquals("Test resources were not available so assertions like case significance and US specific terms checks will not have run.", invalidContents.get(index).getMessage());
 
         index++;
-        assertEquals(Severity.WARNING, invalidContents.get(index).getSeverity());
-        assertEquals("Active FSN should end with a valid semantic tag.", invalidContents.get(index).getMessage());
-
-        index++;
         assertEquals(Severity.ERROR, invalidContents.get(index).getSeverity());
         assertEquals("Active concepts must have at least one IS A relationship.", invalidContents.get(index).getMessage());
 
         index++;
         assertEquals(Severity.WARNING, invalidContents.get(index).getSeverity());
         assertEquals("A concept's semantic tags should be compatible with those of the active parents.", invalidContents.get(index).getMessage());
+
+        index++;
+        assertEquals(Severity.WARNING, invalidContents.get(index).getSeverity());
+        assertEquals("Active FSN should end with a valid semantic tag.", invalidContents.get(index).getMessage());
     }
 
     @Test
@@ -142,13 +122,23 @@ public class DroolValidationServiceTest extends AbstractTest {
         foundConcept.getDescriptions().stream().forEach(description -> description.setAcceptabilityMap(Collections.singletonMap(Concepts.US_EN_LANG_REFSET,
                 Concepts.descriptionAcceptabilityNames.get(PREFERRED))));
 
-        foundConcept.addRelationship(new Relationship("100002", ISA, SNOMEDCT_ROOT).setCharacteristicTypeId(Concepts.STATED_RELATIONSHIP));
+        foundConcept.addRelationship(new Relationship("100002", ISA, SNOMEDCT_ROOT));
 
         ConcreteValue value = ConcreteValue.from("10", "int");
-        foundConcept.addRelationship(new Relationship("100003", "23131313", value));
+        foundConcept.addAxiom(new Relationship("100002", ISA, SNOMEDCT_ROOT), new Relationship("100003", "23131313", value));
 
         final Concept updatedConcept = conceptService.update(foundConcept, DEFAULT_BRANCH);
-        assertEquals(2, updatedConcept.getRelationships().size());
+        assertEquals(1, updatedConcept.getClassAxioms().size());
+
+        Axiom axiom = updatedConcept.getClassAxioms().iterator().next();
+        assertEquals(2, axiom.getRelationships().size());
+
+        for (Relationship relationship : axiom.getRelationships()){
+            if ("100003".equals(relationship.getRelationshipId())) {
+                assertTrue(value.getDataType().equals(relationship.getConcreteValue().getDataType()));
+                assertTrue(value.getValue().equals(relationship.getConcreteValue().getValue()));
+            }
+        }
 
         List <InvalidContent> invalidContents = droolValidationService.validateConcept(DEFAULT_BRANCH, updatedConcept);
         assertEquals(3, invalidContents.size());
@@ -159,34 +149,10 @@ public class DroolValidationServiceTest extends AbstractTest {
 
         index++;
         assertEquals(Severity.WARNING, invalidContents.get(index).getSeverity());
-        assertEquals("Active FSN should end with a valid semantic tag.", invalidContents.get(index).getMessage());
+        assertEquals("A concept's semantic tags should be compatible with those of the active parents.", invalidContents.get(index).getMessage());
 
         index++;
         assertEquals(Severity.WARNING, invalidContents.get(index).getSeverity());
-        assertEquals("A concept's semantic tags should be compatible with those of the active parents.", invalidContents.get(index).getMessage());
-    }
-
-    private void checkoutDroolsRules() throws GitAPIException, IOException {
-        if (checkoutRulesDone) {
-            return;
-        }
-
-        File localRepoDir = new File(droolsRulesPath);
-
-        // Delete old rules if any
-        if (localRepoDir.isDirectory() && localRepoDir.listFiles().length != 0) {
-            FileUtils.deleteDirectory(localRepoDir);
-        }
-
-        logger.info("Cloning snomed-drools-rules repository...");
-        TextProgressMonitor consoleProgressMonitor = new TextProgressMonitor(new PrintWriter(System.out));
-        Repository repo = Git.cloneRepository().setProgressMonitor(consoleProgressMonitor).setDirectory(localRepoDir)
-                .setURI("https://github.com/IHTSDO/snomed-drools-rules.git").call().getRepository();
-        try (Git git = new Git(repo)) {
-            git.checkout().setCreateBranch(true).setName("local-master").setStartPoint("master").call();
-            git.getRepository().close();
-        }
-        droolValidationService.newRuleExecutorAndResources();
-        checkoutRulesDone = true;
+        assertEquals("Active FSN should end with a valid semantic tag.", invalidContents.get(index).getMessage());
     }
 }
