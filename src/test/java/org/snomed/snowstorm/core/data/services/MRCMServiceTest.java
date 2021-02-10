be8fda08dee8c2f26b032680e9a73d8789e11c6a
Tyler Cooksey <Tyler-Cooksey>
2021-02-10 11:59:28 +0000

    CDI-38 - Removed meaningless comments.

--- src/test/java/org/snomed/snowstorm/core/data/services/MRCMServiceTest.java
+++ src/test/java/org/snomed/snowstorm/core/data/services/MRCMServiceTest.java
@@ -67,13 +67,11 @@ class MRCMServiceTest extends AbstractTest {
 	@SuppressWarnings("unchecked")
 	@Test
 	void testExtraConceptMiniFields() throws ServiceException {
-		// create concept
 		final Concept inConcept = new Concept("12345678910");
 		inConcept.addAxiom(new Relationship(ISA, "12345"), Relationship.newConcrete(Concepts.ISA, ConcreteValue.newInteger("#1")));
 		createRangeConstraint(Concepts.ISA, "dec(>#0..)");
 		conceptService.create(inConcept, "MAIN");
 
-		// retrieve domain attributes
 		Collection<ConceptMini> attributes = mrcmService.retrieveDomainAttributes(ContentType.ALL, true,
 				Sets.newHashSet(12345678910L), "MAIN", null);
 		assertNotNull(attributes);
