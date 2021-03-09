6ac451a65d033ef9b384e3d5c99c0980d0198a81
dmcgihtsdo <72863156+dmcgihtsdo@users.noreply.github.com>
2021-03-09 16:51:03 +0000

    MAINT-1372 Fix Snowstorm Log error "No value specified for terms query"
    * MAINT-1372 Ignore null Concept identifiers in bulk-load endpoint to prevent Elasticsearch exception
    
    * MAINT-1372 Ignore null Description identifiers in bulk-load endpoint to prevent Elasticsearch exception

--- src/main/java/org/snomed/snowstorm/rest/pojo/ConceptBulkLoadRequest.java
+++ src/main/java/org/snomed/snowstorm/rest/pojo/ConceptBulkLoadRequest.java
@@ -1,11 +1,9 @@
 package org.snomed.snowstorm.rest.pojo;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.annotation.JsonSetter;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @JsonPropertyOrder({"conceptIds", "descriptionIds"})
 public class ConceptBulkLoadRequest {
@@ -18,11 +16,28 @@ public class ConceptBulkLoadRequest {
 		descriptionIds = new HashSet<>();
 	}
 
+	public ConceptBulkLoadRequest(List<String> conceptIds, Set<String> descriptionIds) {
+		this.conceptIds = conceptIds;
+		this.descriptionIds = descriptionIds;
+	}
+
 	public List<String> getConceptIds() {
 		return conceptIds;
 	}
 
+	@JsonSetter(value = "conceptIds")
+	public void setConceptIdsSafely(List<String> conceptIds) {
+		conceptIds.removeIf(Objects::isNull);
+		this.conceptIds = conceptIds;
+	}
+
 	public Set<String> getDescriptionIds() {
 		return descriptionIds;
+	}
+
+	@JsonSetter(value = "descriptionIds")
+	public void setDescriptionIdsSafely(Set<String> descriptionIds) {
+		descriptionIds.removeIf(Objects::isNull);
+		this.descriptionIds = descriptionIds;
 	}
 }
