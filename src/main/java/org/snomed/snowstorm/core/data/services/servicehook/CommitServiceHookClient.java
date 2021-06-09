b87c90ff239f26f63c7dee9389ffccf8252301ed
Kai Kewley <kaikewley@gmail.com>
2021-06-09 11:14:13 +0100

    FRI-128 Update service hook endpoint to `/integration/snowstorm..`

--- src/main/java/org/snomed/snowstorm/core/data/services/servicehook/CommitServiceHookClient.java
+++ src/main/java/org/snomed/snowstorm/core/data/services/servicehook/CommitServiceHookClient.java
@@ -2,7 +2,6 @@ package org.snomed.snowstorm.core.data.services.servicehook;
 
 import io.kaicode.elasticvc.api.CommitListener;
 import io.kaicode.elasticvc.domain.Commit;
-import org.apache.http.entity.ContentType;
 import org.ihtsdo.sso.integration.SecurityUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,11 +21,6 @@ public class CommitServiceHookClient implements CommitListener {
 	private final RestTemplate restTemplate;
 
 	private final Logger logger = LoggerFactory.getLogger(getClass());
-
-	private static final HttpHeaders JSON_TYPE_HEADER = new HttpHeaders();
-	static {
-		JSON_TYPE_HEADER.setContentType(MediaType.APPLICATION_JSON);
-	}
 
 	public CommitServiceHookClient(@Value("${service-hook.commit.url}") String serviceUrl) {
 
@@ -49,7 +43,7 @@ public class CommitServiceHookClient implements CommitListener {
 			final HttpHeaders httpHeaders = new HttpHeaders();
 			httpHeaders.setContentType(MediaType.APPLICATION_JSON);
 			httpHeaders.add(HttpHeaders.COOKIE, SecurityUtil.getAuthenticationToken());
-			restTemplate.postForEntity("/snowstorm-integration/commit",
+			restTemplate.postForEntity("/integration/snowstorm/commit",
 					new HttpEntity<>(new CommitInformation(commit), httpHeaders), Void.class);
 		} catch (RestClientException e) {
 			logger.warn("Commit service hook failed for branch {}, commit {}.", commit.getBranch().getPath(), commit.getTimepoint().getTime(), e);
