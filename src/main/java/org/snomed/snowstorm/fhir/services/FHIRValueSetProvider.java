5455431c795d057cd1a47495ee4335f6623205fd
pgwilliams <petergwilliams@hotmail.com>
2021-07-01 11:09:13 +0100

    MAINT-1686 Add support for FHIR CS date filtering

--- src/main/java/org/snomed/snowstorm/fhir/services/FHIRValueSetProvider.java
+++ src/main/java/org/snomed/snowstorm/fhir/services/FHIRValueSetProvider.java
@@ -134,7 +134,7 @@ public class FHIRValueSetProvider implements IResourceProvider, FHIRConstants {
 			@OptionalParam(name="context") TokenParam context,
 			@OptionalParam(name="context-quantity") QuantityParam contextQuantity,
 			@OptionalParam(name="context-type") String contextType,
-			@OptionalParam(name="date") String date,
+			@OptionalParam(name="date") StringParam date,
 			@OptionalParam(name="description") StringParam description,
 			@OptionalParam(name="expansion") String expansion,
 			@OptionalParam(name="identifier") StringParam identifier,
