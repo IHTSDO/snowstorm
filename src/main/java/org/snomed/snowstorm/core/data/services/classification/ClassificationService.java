b0b4eec9c12b6a4769d20dd0090d4bec5c2d9352
Darryn McGaw <dmcg@ihtsdo.org>
2021-07-15 17:07:35 +0100

    FRI-123 Add CommitServiceHookClient to ClassificationService
    FRI-123 Remove unnecessary mockCommitCompletion method from ClassificationService

--- src/main/java/org/snomed/snowstorm/core/data/services/classification/ClassificationService.java
+++ src/main/java/org/snomed/snowstorm/core/data/services/classification/ClassificationService.java
@@ -111,6 +111,9 @@ public class ClassificationService {
 	private CommitServiceHookClient commitServiceHookClient;
 
 	@Autowired
+	private CommitServiceHookClient commitServiceHookClient;
+
+	@Autowired
 	private ConceptAttributeSortHelper conceptAttributeSortHelper;
 
 	private final List<Classification> classificationsInProgress;
