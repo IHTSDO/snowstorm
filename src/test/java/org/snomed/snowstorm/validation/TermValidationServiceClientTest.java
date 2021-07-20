efd94a749014c98cae025b49bc39c7e0954b54f2
Kai Kewley <kaikewley@gmail.com>
2021-07-20 15:31:55 +0100

    MAINT-1688 TVS integrate inactive concepts similarity matching.

--- src/test/java/org/snomed/snowstorm/validation/TermValidationServiceClientTest.java
+++ src/test/java/org/snomed/snowstorm/validation/TermValidationServiceClientTest.java
@@ -4,9 +4,9 @@ import org.ihtsdo.drools.response.InvalidContent;
 import org.junit.jupiter.api.Test;
 import org.snomed.snowstorm.core.data.domain.Concept;
 import org.snomed.snowstorm.core.data.domain.Concepts;
-import org.snomed.snowstorm.core.data.services.ServiceException;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.DeserializationFeature;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.PropertyNamingStrategy;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -28,14 +28,21 @@ class TermValidationServiceClientTest {
 		final List<InvalidContent> invalidContents = new ArrayList<>();
 
 		client.handleResponse(objectMapper.readValue(getClass().getResourceAsStream("/term-validation-service/response-1.json"), TermValidationServiceClient.ValidationResponse.class),
-				new Concept(Concepts.CLINICAL_FINDING), invalidContents);
+				new Concept(Concepts.CLINICAL_FINDING), "MAIN/TEST/TEST-123", invalidContents);
 
-		assertEquals(1, invalidContents.size());
 		final Map<String, InvalidContent> idToInvalidContentMap = invalidContents.stream().collect(Collectors.toMap(InvalidContent::getRuleId, Function.identity()));
+		assertEquals(2, invalidContents.size());
+
 		final InvalidContent duplicate = idToInvalidContentMap.get(TermValidationServiceClient.DUPLICATE_RULE_ID);
 		assertNotNull(duplicate);
 		assertEquals("Terms are similar to description 'Atypical diabetes mellitus (disorder)' in concept 530558861000132104. Is this a duplicate?", duplicate.getMessage());
 		assertEquals("404684003", duplicate.getConceptId());
 		assertEquals("404684003", duplicate.getComponentId());// Same as concept
+
+		final InvalidContent similarToInactive = idToInvalidContentMap.get(TermValidationServiceClient.SIMILAR_TO_INACTIVE_RULE_ID);
+		assertNotNull(similarToInactive);
+		assertEquals("Concept is similar to many that have been made inactive in the past with inactivation reason 'Limited'.", similarToInactive.getMessage());
+		assertEquals("404684003", similarToInactive.getConceptId());
+		assertEquals("404684003", similarToInactive.getComponentId());// Same as concept
 	}
 }
