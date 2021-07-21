137d3a836879caf541a84d86c955f212a12975de
Kai Kewley <kaikewley@gmail.com>
2021-07-21 14:48:55 +0100

    MAINT-1688 TVS Integrate FSN coverage check + user messages update.

--- src/main/java/org/snomed/snowstorm/validation/TermValidationServiceClient.java
+++ src/main/java/org/snomed/snowstorm/validation/TermValidationServiceClient.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Iterables;
 import io.kaicode.elasticvc.api.BranchCriteria;
 import io.kaicode.elasticvc.api.VersionControlHelper;
 import org.ihtsdo.drools.response.InvalidContent;
@@ -15,12 +16,14 @@ import org.slf4j.LoggerFactory;
 import org.snomed.snowstorm.config.Config;
 import org.snomed.snowstorm.core.data.domain.Concept;
 import org.snomed.snowstorm.core.data.domain.ConceptMini;
+import org.snomed.snowstorm.core.data.domain.Description;
 import org.snomed.snowstorm.core.data.services.ConceptService;
 import org.snomed.snowstorm.core.data.services.ServiceException;
 import org.snomed.snowstorm.core.pojo.LanguageDialect;
 import org.snomed.snowstorm.core.util.TimerUtil;
 import org.snomed.snowstorm.rest.View;
 import org.snomed.snowstorm.validation.domain.DroolsConcept;
+import org.snomed.snowstorm.validation.domain.DroolsDescription;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -35,12 +38,15 @@ import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 @Service
 public class TermValidationServiceClient {
 
 	public static final String DUPLICATE_RULE_ID = "21c099d0-594f-4473-bc46-d5701fcfac0e";
 	public static final String SIMILAR_TO_INACTIVE_RULE_ID = "fe9a5f26-d9e5-493c-a0c5-0206d4a036a1";
+	public static final String FSN_COVERAGE_RULE_ID = "083b4a5f-6e01-4b78-a37f-11ebaaf8efca";
 
 	private static final HttpHeaders HTTP_HEADERS = new HttpHeaders();
 
@@ -133,8 +139,8 @@ public class TermValidationServiceClient {
 						.max(Comparator.comparing(Match::getScore));
 				if (first.isPresent()) {
 					final Match match = first.get();
-					invalidContents.add(new InvalidContent(DUPLICATE_RULE_ID, new DroolsConcept(concept),
-							String.format("Terms are similar to description '%s' in concept %s. Is this a duplicate?", match.getTerm(), match.getConceptId()),
+					invalidContents.add(new InvalidContent(DUPLICATE_RULE_ID, new DroolsDescription(getEnFsnDescription(concept)),
+							String.format("FSN is similar to description '%s' in concept %s. Is this a duplicate?", match.getTerm(), match.getConceptId()),
 							Severity.WARNING));
 				}
 			}
@@ -146,14 +152,44 @@ public class TermValidationServiceClient {
 				if (highestScoringDetail.isPresent()) {
 					final String inactivationReason = highestScoringDetail.get().getType();
 					invalidContents.add(new InvalidContent(SIMILAR_TO_INACTIVE_RULE_ID, new DroolsConcept(concept),
-							String.format("Concept is similar to many that have been made inactive in the past with inactivation reason '%s'.",	inactivationReason),
+							String.format("This concept is similar to many that have been made inactive in the past with inactivation reason '%s'.",	inactivationReason),
 							Severity.WARNING));
 				} else {
 					logger.warn("TVS indicated that future concept inactivation likely but gave no details so nothing shown to user. Branch {}, concept {}",
 							branchPath, concept.getConceptId());
 				}
 			}
+
+			// FSN coverage
+			final FsnCoverage fsnCoverage = validationResponse.getFsnCoverage();
+			if (fsnCoverage != null) {
+				Set<String> wordsNotCovered = fsnCoverage.getWordCoverage().stream()
+						.filter(Predicate.not(WordCoverage::isPresentAnywhere))
+						.map(WordCoverage::getWord)
+						.collect(Collectors.toSet());
+
+				if (!wordsNotCovered.isEmpty()) {
+					if (wordsNotCovered.size() == 1) {
+						invalidContents.add(new InvalidContent(FSN_COVERAGE_RULE_ID, new DroolsDescription(getEnFsnDescription(concept)),
+								String.format("The word '%s' in the FSN of this defined concept does not occur " +
+										"in the descriptions of any concept in the inferred relationships. Is this correct?", wordsNotCovered.iterator().next()),
+								Severity.WARNING));
+					} else {
+						String lastWord = Iterables.getLast(wordsNotCovered);
+						wordsNotCovered.remove(lastWord);
+						String otherWords = wordsNotCovered.stream().map(s -> String.format("'%s'", s)).collect(Collectors.joining(", "));
+						invalidContents.add(new InvalidContent(FSN_COVERAGE_RULE_ID, new DroolsDescription(getEnFsnDescription(concept)),
+								String.format("The words %s and '%s' in the FSN of this defined concept do not occur " +
+										"in the descriptions of any concept in the inferred relationships. Is this correct?", otherWords, lastWord),
+								Severity.WARNING));
+					}
+				}
+			}
 		}
+	}
+
+	private Description getEnFsnDescription(Concept concept) {
+		return concept.getDescriptions().stream().filter(d -> d.getLang().equals("en")).findFirst().orElse(null);
 	}
 
 	public ObjectMapper getObjectMapper() {
@@ -190,6 +226,8 @@ public class TermValidationServiceClient {
 		// Measure similarity to previously inactivated concepts
 		private ModelPrediction modelPrediction;
 
+		private FsnCoverage fsnCoverage;
+
 		public Duplication getDuplication() {
 			return duplication;
 		}
@@ -198,8 +236,8 @@ public class TermValidationServiceClient {
 			return modelPrediction;
 		}
 
-		public void setModelPrediction(ModelPrediction modelPrediction) {
-			this.modelPrediction = modelPrediction;
+		public FsnCoverage getFsnCoverage() {
+			return fsnCoverage;
 		}
 	}
 
@@ -265,6 +303,39 @@ public class TermValidationServiceClient {
 
 		public String getType() {
 			return type;
+		}
+	}
+
+	public static final class FsnCoverage {
+
+		private List<WordCoverage> wordCoverage;
+
+		public List<WordCoverage> getWordCoverage() {
+			return wordCoverage;
+		}
+	}
+
+	public static final class WordCoverage {
+
+		private String word;
+		private String pos;
+		private boolean samePosPresent;
+		private boolean presentAnywhere;
+
+		public String getWord() {
+			return word;
+		}
+
+		public String getPos() {
+			return pos;
+		}
+
+		public boolean isSamePosPresent() {
+			return samePosPresent;
+		}
+
+		public boolean isPresentAnywhere() {
+			return presentAnywhere;
 		}
 	}
 }
