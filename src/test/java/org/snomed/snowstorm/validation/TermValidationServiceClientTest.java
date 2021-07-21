2ebaefe4c237aa721c136169ecfe6e1555a4d508
Kai Kewley <kaikewley@gmail.com>
2021-07-21 14:48:55 +0100

    MAINT-1688 TVS Integrate FSN coverage check + user messages update.

--- src/test/java/org/snomed/snowstorm/validation/TermValidationServiceClientTest.java
+++ src/test/java/org/snomed/snowstorm/validation/TermValidationServiceClientTest.java
@@ -4,8 +4,7 @@ import org.ihtsdo.drools.response.InvalidContent;
 import org.junit.jupiter.api.Test;
 import org.snomed.snowstorm.core.data.domain.Concept;
 import org.snomed.snowstorm.core.data.domain.Concepts;
-import org.testcontainers.shaded.com.fasterxml.jackson.databind.DeserializationFeature;
-import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+import org.snomed.snowstorm.core.data.domain.Description;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -25,23 +24,38 @@ class TermValidationServiceClientTest {
 	void testHandleResponse() throws IOException {
 		final List<InvalidContent> invalidContents = new ArrayList<>();
 
-		client.handleResponse(client.getObjectMapper().readValue(getClass().getResourceAsStream("/term-validation-service/response-1.json"),
+		// The content of this response is intensionally inconsistent.
+		// It's made from parts of different example responses that trigger all the error cases.
+		final String dummyResponse = "/term-validation-service/response-1.json";
+
+		client.handleResponse(client.getObjectMapper().readValue(getClass().getResourceAsStream(dummyResponse),
 				TermValidationServiceClient.ValidationResponse.class),
-				new Concept(Concepts.CLINICAL_FINDING), "MAIN/TEST/TEST-123", invalidContents);
+				new Concept(Concepts.CLINICAL_FINDING).addDescription(
+						new Description("2148514019", "Clinical finding (finding)")
+								.setTypeId(Concepts.FSN)
+								.addLanguageRefsetMember(Concepts.US_EN_LANG_REFSET, Concepts.PREFERRED)),
+				"MAIN/TEST/TEST-123", invalidContents);
 
 		final Map<String, InvalidContent> idToInvalidContentMap = invalidContents.stream().collect(Collectors.toMap(InvalidContent::getRuleId, Function.identity()));
-		assertEquals(2, invalidContents.size());
+		assertEquals(3, invalidContents.size());
 
 		final InvalidContent duplicate = idToInvalidContentMap.get(TermValidationServiceClient.DUPLICATE_RULE_ID);
 		assertNotNull(duplicate);
-		assertEquals("Terms are similar to description 'Atypical diabetes mellitus (disorder)' in concept 530558861000132104. Is this a duplicate?", duplicate.getMessage());
+		assertEquals("FSN is similar to description 'Atypical diabetes mellitus (disorder)' in concept 530558861000132104. Is this a duplicate?", duplicate.getMessage());
 		assertEquals("404684003", duplicate.getConceptId());
-		assertEquals("404684003", duplicate.getComponentId());// Same as concept
+		assertEquals("2148514019", duplicate.getComponentId());// FSN
 
 		final InvalidContent similarToInactive = idToInvalidContentMap.get(TermValidationServiceClient.SIMILAR_TO_INACTIVE_RULE_ID);
 		assertNotNull(similarToInactive);
-		assertEquals("Concept is similar to many that have been made inactive in the past with inactivation reason 'Limited'.", similarToInactive.getMessage());
+		assertEquals("This concept is similar to many that have been made inactive in the past with inactivation reason 'Limited'.", similarToInactive.getMessage());
 		assertEquals("404684003", similarToInactive.getConceptId());
 		assertEquals("404684003", similarToInactive.getComponentId());// Same as concept
+
+		final InvalidContent fsnCoverage = idToInvalidContentMap.get(TermValidationServiceClient.FSN_COVERAGE_RULE_ID);
+		assertNotNull(fsnCoverage);
+		assertEquals("The word 'left' in the FSN of this defined concept does not occur in the descriptions of any concept in the inferred relationships. Is this correct?",
+				fsnCoverage.getMessage());
+		assertEquals("404684003", fsnCoverage.getConceptId());
+		assertEquals("2148514019", fsnCoverage.getComponentId());// FSN
 	}
 }
