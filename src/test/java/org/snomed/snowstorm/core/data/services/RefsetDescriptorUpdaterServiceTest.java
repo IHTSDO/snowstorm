397cbceb6ad5c127b0591a5af7e0d9609e408d30
dmcgihtsdo <72863156+dmcgihtsdo@users.noreply.github.com>
2021-11-02 21:09:04 +0000

    FRI-262 Prevent duplicates being written to descriptor refset
    * FRI-262 Add overloaded findMembers method to ReferenceSetMemberService
    
    * FRI-262 Change RefsetDescriptorUpdaterService to update members when not creating Concept in commit
    
    * FRI-262 Remove overloaded findMembers method from ReferenceSetMemberService
    
    * FRI-262 Remove top-level condition check from RefsetDescriptorUpdaterService
    
    * FRI-262 Remove doUpdateRefsetMembers logic from RefsetDescriptorUpdaterService
    
    * FRI-262 Remove moduleId setters on Relationships

--- src/test/java/org/snomed/snowstorm/core/data/services/RefsetDescriptorUpdaterServiceTest.java
+++ src/test/java/org/snomed/snowstorm/core/data/services/RefsetDescriptorUpdaterServiceTest.java
@@ -19,6 +19,8 @@ import static org.junit.Assert.assertEquals;
 
 @ExtendWith(SpringExtension.class)
 class RefsetDescriptorUpdaterServiceTest extends AbstractTest {
+	private static final PageRequest PAGE_REQUEST = PageRequest.of(0, 10);
+
 	@Autowired
 	private ReferenceSetMemberService memberService;
 
@@ -32,7 +34,7 @@ class RefsetDescriptorUpdaterServiceTest extends AbstractTest {
 		conceptService.create(new Concept(Concepts.ISA)
 				.addAxiom(new Relationship(Concepts.ISA, Concepts.SNOMEDCT_ROOT)), MAIN);
 		conceptService.create(new Concept(Concepts.REFSET)
-				.addAxiom(new Relationship(Concepts.ISA, Concepts.FOUNDATION_METADATA)), MAIN);
+				.addAxiom(new Relationship(Concepts.ISA, Concepts.FOUNDATION_METADATA)).setModuleId(Concepts.MODEL_MODULE), MAIN);
 		conceptService.create(new Concept(Concepts.CLINICAL_FINDING)
 				.addAxiom(new Relationship(Concepts.ISA, Concepts.SNOMEDCT_ROOT)), MAIN);
 		conceptService.create(new Concept(Concepts.REFSET_HISTORICAL_ASSOCIATION)
@@ -40,9 +42,9 @@ class RefsetDescriptorUpdaterServiceTest extends AbstractTest {
 		conceptService.create(new Concept(Concepts.REFSET_POSSIBLY_EQUIVALENT_TO_ASSOCIATION)
 				.addAxiom(new Relationship(Concepts.ISA, Concepts.REFSET_HISTORICAL_ASSOCIATION)), MAIN);
 		conceptService.create(new Concept(Concepts.REFSET_SIMPLE)
-				.addAxiom(new Relationship(Concepts.ISA, Concepts.REFSET)), MAIN);
+				.addAxiom(new Relationship(Concepts.ISA, Concepts.REFSET)).setModuleId(Concepts.MODEL_MODULE), MAIN);
 		conceptService.create(new Concept(Concepts.REFSET_DESCRIPTOR_REFSET)
-				.addAxiom(new Relationship(Concepts.ISA, Concepts.REFSET)), MAIN);
+				.addAxiom(new Relationship(Concepts.ISA, Concepts.REFSET)).setModuleId(Concepts.MODEL_MODULE), MAIN);
 	}
 
 	@Test
@@ -62,6 +64,35 @@ class RefsetDescriptorUpdaterServiceTest extends AbstractTest {
 
 		assertEquals(2, membersAfter.size()); // simple & newRefSet
 		assertEquals(newRefSet.getId(), referenceSetMember.getReferencedComponentId());
+		assertEquals(Concepts.REFSET_DESCRIPTOR_REFSET, referenceSetMember.getRefsetId());
+		assertEquals(Concepts.REFERENCED_COMPONENT, referenceSetMember.getAdditionalField("attributeDescription"));
+		assertEquals(Concepts.CONCEPT_TYPE_COMPONENT, referenceSetMember.getAdditionalField("attributeType"));
+		assertEquals("0", referenceSetMember.getAdditionalField("attributeOrder"));
+	}
+
+	@Test
+	void testUpdatingRefSetConcept() throws ServiceException {
+		// Create & assert initial seed data
+		givenRefSetAncestorsExist();
+		assertEquals(1, memberService.findMembers("MAIN", buildMemberSearchRequest(true, Concepts.REFSET_DESCRIPTOR_REFSET), PAGE_REQUEST).getContent().size());
+
+		// Create & assert two test reference sets
+		Concept newRefSet1 = createNewRefSet();
+		Concept newRefSet2 = createNewRefSet();
+		assertEquals(3, memberService.findMembers("MAIN", buildMemberSearchRequest(true, Concepts.REFSET_DESCRIPTOR_REFSET), PAGE_REQUEST).getContent().size());
+
+		// Change modelling for semantic index change. newRefSet2 now |is a| newRefSet1; previously it was a simple reference set.
+		changeIsATarget(newRefSet1, newRefSet2);
+
+		// when
+		conceptService.update(newRefSet2, "MAIN");
+
+		// then
+		List<ReferenceSetMember> members = memberService.findMembers("MAIN", buildMemberSearchRequest(true, Concepts.REFSET_DESCRIPTOR_REFSET), PAGE_REQUEST).getContent();
+		ReferenceSetMember referenceSetMember = members.get(2); // newRefSet2 is 3rd document.
+
+		assertEquals(3, members.size());
+		assertEquals(newRefSet2.getId(), referenceSetMember.getReferencedComponentId());
 		assertEquals(Concepts.REFSET_DESCRIPTOR_REFSET, referenceSetMember.getRefsetId());
 		assertEquals(Concepts.REFERENCED_COMPONENT, referenceSetMember.getAdditionalField("attributeDescription"));
 		assertEquals(Concepts.CONCEPT_TYPE_COMPONENT, referenceSetMember.getAdditionalField("attributeType"));
@@ -100,11 +131,13 @@ class RefsetDescriptorUpdaterServiceTest extends AbstractTest {
 
 		Axiom axiom = new Axiom();
 		axiom.setRelationships(relationships);
+		axiom.setModuleId(Concepts.MODEL_MODULE);
 		Set<Axiom> axioms = new HashSet<>();
 		axioms.add(axiom);
 
 		Concept concept = new Concept();
 		concept.setClassAxioms(axioms);
+		concept.setModuleId(Concepts.MODEL_MODULE);
 
 		return conceptService.create(concept, "MAIN");
 	}
@@ -117,12 +150,19 @@ class RefsetDescriptorUpdaterServiceTest extends AbstractTest {
 		return memberSearchRequest;
 	}
 
-	private MemberSearchRequest buildMemberSearchRequest(boolean active, String referenceSetId, String referencedComponentId) {
-		MemberSearchRequest memberSearchRequest = new MemberSearchRequest();
-		memberSearchRequest.active(active);
-		memberSearchRequest.referenceSet(referenceSetId);
-		memberSearchRequest.referencedComponentId(referencedComponentId);
+	private void changeIsATarget(Concept newRefSet1, Concept newRefSet2) {
+		Relationship relationship = new Relationship();
+		relationship.setTypeId(Concepts.ISA);
+		relationship.setDestinationId(newRefSet1.getConceptId());
+		Set<Relationship> relationships = new HashSet<>();
+		relationships.add(relationship);
 
-		return memberSearchRequest;
+		Axiom axiom = new Axiom();
+		axiom.setRelationships(relationships);
+		axiom.setModuleId(Concepts.MODEL_MODULE);
+		Set<Axiom> axioms = new HashSet<>();
+		axioms.add(axiom);
+
+		newRefSet2.setRelationships(relationships);
 	}
 }
