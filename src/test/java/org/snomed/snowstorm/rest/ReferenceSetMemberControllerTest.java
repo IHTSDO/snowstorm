05baad8a0558043350d80b1affbd500bf3bd0f7b
Darryn McGaw <dmcg@ihtsdo.org>
2021-11-04 14:25:32 +0000

    MAINT-1790 Add test for same result returned from different paging mechanisms

--- src/test/java/org/snomed/snowstorm/rest/ReferenceSetMemberControllerTest.java
+++ src/test/java/org/snomed/snowstorm/rest/ReferenceSetMemberControllerTest.java
@@ -16,6 +16,10 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
@@ -64,6 +68,33 @@ class ReferenceSetMemberControllerTest extends AbstractTest {
 		assertNotEquals(firstPageMember, secondPageMember);
 	}
 
+	@Test
+	void findRefsetMembers_ShouldReturnSameMember_WhenUsingDifferentPagingMechanisms() {
+		// given
+		Set<ReferenceSetMember> referenceSetMembers = new HashSet<>();
+		Collections.addAll(
+				referenceSetMembers,
+				// Members are assigned UUIDs & returned in descending order. Cannot know order returned from API beforehand for more specific assertions.
+				new ReferenceSetMember(Concepts.MODEL_MODULE, Concepts.REFSET_DESCRIPTOR_REFSET, Concepts.REFSET_MOVED_FROM_ASSOCIATION),
+				new ReferenceSetMember(Concepts.MODEL_MODULE, Concepts.REFSET_DESCRIPTOR_REFSET, Concepts.REFSET_MOVED_TO_ASSOCIATION),
+				new ReferenceSetMember(Concepts.MODEL_MODULE, Concepts.REFSET_DESCRIPTOR_REFSET, Concepts.REFSET_HISTORICAL_ASSOCIATION),
+				new ReferenceSetMember(Concepts.MODEL_MODULE, Concepts.REFSET_DESCRIPTOR_REFSET, Concepts.REFSET_MODULE_DEPENDENCY),
+				new ReferenceSetMember(Concepts.MODEL_MODULE, Concepts.REFSET_DESCRIPTOR_REFSET, Concepts.REFSET_SIMPLE)
+		);
+		referenceSetMemberService.createMembers("MAIN", referenceSetMembers);
+		ItemsPagePojo<ReferenceSetMember> searchAfterTokenRequest = findRefsetMembers(0, 1);
+
+		// when
+		ItemsPagePojo<ReferenceSetMember> firstRequest = findRefsetMembers(searchAfterTokenRequest.getSearchAfter(), 1); // Requesting second entry (skipping first via searchAfter token)
+		ItemsPagePojo<ReferenceSetMember> secondRequest = findRefsetMembers(1, 1); // Requesting second entry (skipping first via offset)
+
+		// then
+		ReferenceSetMember firstRequestMember = getReferenceSetMember(firstRequest);
+		ReferenceSetMember secondRequestMember = getReferenceSetMember(secondRequest);
+		assertEquals(firstRequestMember.getMemberId(), secondRequestMember.getMemberId());
+		assertEquals(firstRequestMember.getReferencedComponentId(), secondRequestMember.getReferencedComponentId());
+	}
+
 	private ReferenceSetMember getReferenceSetMember(ItemsPagePojo<ReferenceSetMember> firstPageMembers) {
 		return firstPageMembers.getItems().iterator().next();
 	}
@@ -78,5 +109,9 @@ class ReferenceSetMemberControllerTest extends AbstractTest {
 
 	private ItemsPagePojo<ReferenceSetMember> findRefsetMembers(String searchAfter) {
 		return this.restTemplate.exchange("http://localhost:" + port + "/MAIN/members?searchAfter=" + searchAfter, HttpMethod.GET, new HttpEntity<>(null), FIND_REFSET_MEMBERS_RESPONSE_TYPE).getBody();
+	}
+
+	private ItemsPagePojo<ReferenceSetMember> findRefsetMembers(String searchAfter, int limit) {
+		return this.restTemplate.exchange("http://localhost:" + port + "/MAIN/members?searchAfter=" + searchAfter + "&limit=" + limit, HttpMethod.GET, new HttpEntity<>(null), FIND_REFSET_MEMBERS_RESPONSE_TYPE).getBody();
 	}
 }
