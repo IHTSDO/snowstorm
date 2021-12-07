6b3ddd8b60670ee38f642911d37f4b20c1364368
Darryn McGaw <dmcg@ihtsdo.org>
2021-12-07 14:35:36 +0000

    FRI-27 Conditionally retrieve classification status via JMS

--- src/main/java/org/snomed/snowstorm/core/data/services/classification/pojo/ClassificationStatusResponse.java
+++ src/main/java/org/snomed/snowstorm/core/data/services/classification/pojo/ClassificationStatusResponse.java
@@ -5,10 +5,18 @@ import org.snomed.snowstorm.core.data.domain.classification.ClassificationStatus
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ClassificationStatusResponse {
-
+	private String id;
 	private ClassificationStatus status;
 	private String errorMessage;
 	private String developerMessage;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
 
 	public ClassificationStatus getStatus() {
 		return status;
