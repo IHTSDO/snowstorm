20049906ae12392ae34e5c7f930eb21d910b64a9
Kai Kewley <kaikewley@gmail.com>
2022-02-16 20:59:14 +0000

    FHIR: Upgrade to HAPI 5.6.2.

--- src/main/java/org/snomed/snowstorm/fhir/config/HapiRestfulServlet.java
+++ src/main/java/org/snomed/snowstorm/fhir/config/HapiRestfulServlet.java
@@ -1,32 +1,22 @@
 package org.snomed.snowstorm.fhir.config;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.servlet.ServletException;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.snomed.snowstorm.fhir.services.FHIRCodeSystemProvider;
-import org.snomed.snowstorm.fhir.services.FHIRConceptMapProvider;
-import org.snomed.snowstorm.fhir.services.FHIRMedicationProvider;
-import org.snomed.snowstorm.fhir.services.FHIRStructureDefinitionProvider;
-import org.snomed.snowstorm.fhir.services.FHIRTerminologyCapabilitiesProvider;
-import org.snomed.snowstorm.fhir.services.FHIRValueSetProvider;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
-
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.parser.LenientErrorHandler;
 import ca.uhn.fhir.parser.StrictErrorHandler;
 import ca.uhn.fhir.rest.api.EncodingEnum;
-import ca.uhn.fhir.rest.server.IResourceProvider;
 import ca.uhn.fhir.rest.server.RestfulServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.snomed.snowstorm.fhir.services.*;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import javax.servlet.ServletException;
 
 public class HapiRestfulServlet extends RestfulServer {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private final Logger logger = LoggerFactory.getLogger(getClass());
 
 	/**
@@ -40,51 +30,45 @@ public class HapiRestfulServlet extends RestfulServer {
 
 		setDefaultResponseEncoding(EncodingEnum.JSON);
 
-		final FhirContext ctxt = FhirContext.forR4();
+		final FhirContext fhirContext = FhirContext.forR4();
 		final LenientErrorHandler delegateHandler = new LenientErrorHandler();
-		ctxt.setParserErrorHandler(new StrictErrorHandler() {
+		fhirContext.setParserErrorHandler(new StrictErrorHandler() {
 			@Override
 			public void unknownAttribute(IParseLocation theLocation, String theAttributeName) {
 				delegateHandler.unknownAttribute(theLocation, theAttributeName);
 			}
+
 			@Override
 			public void unknownElement(IParseLocation theLocation, String theElementName) {
 				delegateHandler.unknownElement(theLocation, theElementName);
 			}
+
 			@Override
 			public void unknownReference(IParseLocation theLocation, String theReference) {
 				delegateHandler.unknownReference(theLocation, theReference);
 			}
 		});
-		setFhirContext(ctxt);
+		setFhirContext(fhirContext);
+
+		FHIRHelper fhirHelper = applicationContext.getBean(FHIRHelper.class);
+		fhirHelper.setFhirContext(fhirContext);
 
 		/*
 		 * The servlet defines any number of resource providers, and configures itself to use them by calling
 		 * setResourceProviders()
 		 */
-		List<IResourceProvider> resourceProviders = new ArrayList<>();
-		FHIRCodeSystemProvider csp = applicationContext.getBean(FHIRCodeSystemProvider.class);
-		FHIRValueSetProvider vsp = applicationContext.getBean(FHIRValueSetProvider.class);
-		FHIRConceptMapProvider cmp = applicationContext.getBean(FHIRConceptMapProvider.class);
-		FHIRMedicationProvider mp = applicationContext.getBean(FHIRMedicationProvider.class);
-		FHIRStructureDefinitionProvider sd = applicationContext.getBean(FHIRStructureDefinitionProvider.class);
-		
-		resourceProviders.add(csp);
-		resourceProviders.add(vsp);
-		resourceProviders.add(cmp);
-		resourceProviders.add(mp);
-		resourceProviders.add(sd);
-		
-		setResourceProviders(resourceProviders);
-		
-		FHIRTerminologyCapabilitiesProvider tcp = applicationContext.getBean(FHIRTerminologyCapabilitiesProvider.class);
-		setServerConformanceProvider(tcp);
-		
-		// Now register interceptors
-		RootInterceptor interceptor = new RootInterceptor();
-		registerInterceptor(interceptor);
-		
+		setResourceProviders(
+				applicationContext.getBean(FHIRCodeSystemProvider.class),
+				applicationContext.getBean(FHIRValueSetProvider.class),
+				applicationContext.getBean(FHIRConceptMapProvider.class),
+				applicationContext.getBean(FHIRMedicationProvider.class),
+				applicationContext.getBean(FHIRStructureDefinitionProvider.class));
+
+		setServerConformanceProvider(new FHIRTerminologyCapabilitiesProvider(this));
+
+		// Register interceptors
+		registerInterceptor(new RootInterceptor());
+
 		logger.info("FHIR Resource providers and interceptors registered");
 	}
 }
-
--- src/main/java/org/snomed/snowstorm/fhir/services/FHIRTerminologyCapabilitiesProvider.java
+++ src/main/java/org/snomed/snowstorm/fhir/services/FHIRTerminologyCapabilitiesProvider.java
@@ -2,14 +2,18 @@ package org.snomed.snowstorm.fhir.services;
 
 import javax.servlet.http.HttpServletRequest;
 
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.context.support.IValidationSupport;
+import ca.uhn.fhir.rest.server.RestfulServer;
+import ca.uhn.fhir.rest.server.RestfulServerConfiguration;
+import ca.uhn.fhir.rest.server.provider.ServerCapabilityStatementProvider;
+import ca.uhn.fhir.rest.server.util.ISearchParamRegistry;
 import org.hl7.fhir.instance.model.api.IBaseConformance;
-import org.hl7.fhir.r4.hapi.rest.server.ServerCapabilityStatementProvider;
 import org.springframework.stereotype.Component;
 
 import ca.uhn.fhir.rest.annotation.Metadata;
 import ca.uhn.fhir.rest.api.server.RequestDetails;
 
-@Component
 /**
  * See https://www.hl7.org/fhir/terminologycapabilities.html
  * See https://smilecdr.com/hapi-fhir/docs/server_plain/introduction.html#capability-statement-server-metadata
@@ -17,7 +21,19 @@ import ca.uhn.fhir.rest.api.server.RequestDetails;
  * See https://github.com/jamesagnew/hapi-fhir/issues/1681
  */
 public class FHIRTerminologyCapabilitiesProvider extends ServerCapabilityStatementProvider {
-	
+
+	public FHIRTerminologyCapabilitiesProvider(RestfulServer theServer) {
+		super(theServer);
+	}
+
+	public FHIRTerminologyCapabilitiesProvider(FhirContext theContext, RestfulServerConfiguration theServerConfiguration) {
+		super(theContext, theServerConfiguration);
+	}
+
+	public FHIRTerminologyCapabilitiesProvider(RestfulServer theRestfulServer, ISearchParamRegistry theSearchParamRegistry, IValidationSupport theValidationSupport) {
+		super(theRestfulServer, theSearchParamRegistry, theValidationSupport);
+	}
+
 	@Metadata
 	public IBaseConformance getMetadataResource(HttpServletRequest request, RequestDetails requestDetails) {
 		if (request.getParameter("mode") != null && request.getParameter("mode").equals("terminology")) {
