d80d2b2d6bf66a87e9716ce6a6e0703f9cb1ea19
Kai Kewley <kaikewley@gmail.com>
2022-06-17 16:51:52 +0100

    FHIR: ConceptMap translate - any codesystem.

--- src/test/java/org/snomed/snowstorm/fhir/services/FHIRConceptMapProviderTest.java
+++ src/test/java/org/snomed/snowstorm/fhir/services/FHIRConceptMapProviderTest.java
@@ -12,18 +12,17 @@ class FHIRConceptMapProviderTest extends AbstractFHIRTest {
 	
 	@Test
 	void testHistoricAssociation() throws FHIROperationException {
-		//We're using MAIN so pass the unversioned version
-		String vs = "http://snomed.info/sct/900000000000207008/version/UNVERSIONED?fhir_cm=" + Concepts.REFSET_SAME_AS_ASSOCIATION;
-		String sourceTarget = "source=http://snomed.info/sct?fhir_vs&target=http://snomed.info/sct?fhir_vs";
-		String url = "http://localhost:" + port + "/fhir/ConceptMap/$translate?code=" + sampleSCTID + "&system=http://snomed.info/sct&" + sourceTarget + "&url=" + vs;
+		// Use xsct to access daily build
+		String vs = "http://snomed.info/xsct?fhir_cm=" + Concepts.REFSET_SAME_AS_ASSOCIATION;
+		String url = "http://localhost:" + port + "/fhir/ConceptMap/$translate?code=" + sampleSCTID + "&system=http://snomed.info/xsct&url=" + vs;
 		Parameters parameters = getParameters(url);
 		assertNotNull(parameters);
 		Type t = parameters.getParameter("result");
 		assertTrue(t.castToBoolean(t).booleanValue());
-		
-		//We can also use the xsct form to specify unpublished content
-		vs = "http://snomed.info/xsct?fhir_cm=" + Concepts.REFSET_SAME_AS_ASSOCIATION;
-		url = "http://localhost:" + port + "/fhir/ConceptMap/$translate?code=" + sampleSCTID + "&system=http://snomed.info/sct&" + sourceTarget + "&url=" + vs;
+
+		// Should also work with a specific module
+		vs = "http://snomed.info/xsct/1234000008?fhir_cm=" + Concepts.REFSET_SAME_AS_ASSOCIATION;
+		url = "http://localhost:" + port + "/fhir/ConceptMap/$translate?code=" + sampleSCTID + "&system=http://snomed.info/sct&url=" + vs;
 		parameters = getParameters(url);
 		assertNotNull(parameters);
 		t = parameters.getParameter("result");
