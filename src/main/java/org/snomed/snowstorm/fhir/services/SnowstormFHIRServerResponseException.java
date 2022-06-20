ba82eff564dfe45e63a09394f0d7fc66fc1ef365
Kai Kewley <kaikewley@gmail.com>
2022-06-20 10:45:44 +0100

    FHIR: Continue import if a map or valueset fails.

---
+++ src/main/java/org/snomed/snowstorm/fhir/services/SnowstormFHIRServerResponseException.java
@@ -0,0 +1,10 @@
+package org.snomed.snowstorm.fhir.services;
+
+import ca.uhn.fhir.rest.server.exceptions.BaseServerResponseException;
+import org.hl7.fhir.instance.model.api.IBaseOperationOutcome;
+
+public class SnowstormFHIRServerResponseException extends BaseServerResponseException {
+	public SnowstormFHIRServerResponseException(int theStatusCode, String theMessage, IBaseOperationOutcome theBaseOperationOutcome) {
+		super(theStatusCode, theMessage, theBaseOperationOutcome);
+	}
+}
