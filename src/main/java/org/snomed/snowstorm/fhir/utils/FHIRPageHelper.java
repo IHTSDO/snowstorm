edf541575e450a76a8b6e0ee2666990fe03fb3ae
Kai Kewley <kaikewley@gmail.com>
2022-06-20 13:08:28 +0100

    FHIR: Set ValueSet find all page size.

--- src/main/java/org/snomed/snowstorm/fhir/utils/FHIRPageHelper.java
+++ src/main/java/org/snomed/snowstorm/fhir/utils/FHIRPageHelper.java
@@ -3,6 +3,7 @@ package org.snomed.snowstorm.fhir.utils;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.core.SearchHit;
 import org.springframework.data.elasticsearch.core.SearchHits;
 
@@ -10,7 +11,7 @@ import java.util.stream.Collectors;
 
 public class FHIRPageHelper {
 	@NotNull
-	public static <T> PageImpl<T> toPage(SearchHits<T> searchHits, PageRequest pageRequest) {
+	public static <T> PageImpl<T> toPage(SearchHits<T> searchHits, Pageable pageRequest) {
 		return new PageImpl<>(searchHits.get().map(SearchHit::getContent).collect(Collectors.toList()), pageRequest, searchHits.getTotalHits());
 	}
 }
