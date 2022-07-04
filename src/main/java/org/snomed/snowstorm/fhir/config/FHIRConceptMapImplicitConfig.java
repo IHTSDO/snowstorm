63cc846c860d9b4ad28afa1bcc0670adce5910cb
Kai Kewley <kaikewley@gmail.com>
2022-07-04 13:57:44 +0100

    FHIR: Implicit comp. maps, equivalence with config

--- src/main/java/org/snomed/snowstorm/fhir/config/FHIRConceptMapImplicitConfig.java
+++ src/main/java/org/snomed/snowstorm/fhir/config/FHIRConceptMapImplicitConfig.java
@@ -1,5 +1,7 @@
 package org.snomed.snowstorm.fhir.config;
 
+import org.hl7.fhir.r4.model.Enumerations;
+import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.snomed.snowstorm.fhir.pojo.FHIRSnomedConceptMapConfig;
@@ -13,11 +15,9 @@ public class FHIRConceptMapImplicitConfig {
 
 	private Map<String, String> snomedImplicit = new HashMap<>();
 
-	private final Logger logger = LoggerFactory.getLogger(getClass());
+	private Map<String, String> snomedImplicitEquivalence = new HashMap<>();
 
-	public Map<String, String> getSnomedImplicit() {
-		return snomedImplicit;
-	}
+	private final Logger logger = LoggerFactory.getLogger(getClass());
 
 	public List<FHIRSnomedConceptMapConfig> getImplicitMaps() {
 		List<FHIRSnomedConceptMapConfig> maps = new ArrayList<>();
@@ -32,9 +32,41 @@ public class FHIRConceptMapImplicitConfig {
 			String sourceSystem = split[1];
 			String targetSystem = split[2];
 			String equivalence = split.length == 4 ? split[3] : null;
+			if (equivalence != null) {
+				equivalenceCodeToEnumOrLogError(equivalence);
+			}
 			maps.add(new FHIRSnomedConceptMapConfig(refsetId, name, sourceSystem, targetSystem, equivalence));
 		}
 		logger.info("{} implicit FHIR ConceptMaps configured for SNOMED CT.", maps.size());
 		return maps;
+	}
+
+	public Map<String, Enumerations.ConceptMapEquivalence> getSnomedCorrelationToFhirEquivalenceMap() {
+		HashMap<String, Enumerations.ConceptMapEquivalence> map = new HashMap<>();
+
+		for (Map.Entry<String, String> entry : snomedImplicitEquivalence.entrySet()) {
+			String equivalenceString = entry.getValue();
+			Enumerations.ConceptMapEquivalence equivalenceEnum = equivalenceCodeToEnumOrLogError(equivalenceString);
+			map.put(entry.getKey(), equivalenceEnum);
+		}
+
+		return map;
+	}
+
+	@Nullable
+	private Enumerations.ConceptMapEquivalence equivalenceCodeToEnumOrLogError(String equivalenceString) {
+		Enumerations.ConceptMapEquivalence equivalenceEnum = Enumerations.ConceptMapEquivalence.fromCode(equivalenceString);
+		if (equivalenceEnum == null) {
+			logger.error("Configured FHIR MAP equivalence value '{}' not recognised in R4. Please check configuration.", equivalenceString);
+		}
+		return equivalenceEnum;
+	}
+
+	public Map<String, String> getSnomedImplicit() {
+		return snomedImplicit;
+	}
+
+	public Map<String, String> getSnomedImplicitEquivalence() {
+		return snomedImplicitEquivalence;
 	}
 }
