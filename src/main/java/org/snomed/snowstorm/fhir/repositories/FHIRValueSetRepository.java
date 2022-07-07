065699f5dd5d7912d19bd43d04f8489554d39840
Kai Kewley <kaikewley@gmail.com>
2022-07-07 17:47:12 +0100

    FHIR: ValueSet maintenance fixes.

--- src/main/java/org/snomed/snowstorm/fhir/repositories/FHIRValueSetRepository.java
+++ src/main/java/org/snomed/snowstorm/fhir/repositories/FHIRValueSetRepository.java
@@ -3,10 +3,13 @@ package org.snomed.snowstorm.fhir.repositories;
 import org.snomed.snowstorm.fhir.domain.FHIRValueSet;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FHIRValueSetRepository extends ElasticsearchRepository<FHIRValueSet, String> {
 
 	Optional<FHIRValueSet> findByUrl(String url);
+
+	List<FHIRValueSet> findAllByUrl(String url);
 
 }
