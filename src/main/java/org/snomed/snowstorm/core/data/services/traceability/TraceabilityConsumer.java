af8622560b1f17d792c7b2faf549d0022424e268
Mark Rijnbeek <markrynbeek@gmail.com>
2022-08-22 14:13:22 +0200

    FRI-443 Batch processing N concept changes per JMS message

--- src/main/java/org/snomed/snowstorm/core/data/services/traceability/TraceabilityConsumer.java
+++ src/main/java/org/snomed/snowstorm/core/data/services/traceability/TraceabilityConsumer.java
@@ -1,10 +1,16 @@
 package org.snomed.snowstorm.core.data.services.traceability;
 
+import com.google.common.collect.Iterables;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
+
+import java.util.Iterator;
+import java.util.List;
 
 @Component
 @Lazy
@@ -13,11 +19,37 @@ public class TraceabilityConsumer {
 	@Value("${jms.queue.prefix}")
 	private String jmsQueuePrefix;
 
+	@Value("${activemq.max.message.concept-activities}")
+	private int maxConceptActiviesPerMessage;
+
 	@Autowired
 	private JmsTemplate jmsTemplate;
 
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+
 	public void accept(Activity activity) {
-		jmsTemplate.convertAndSend(jmsQueuePrefix + ".traceability", activity);
+		if (activity.getChanges().size() <= maxConceptActiviesPerMessage) {
+			jmsTemplate.convertAndSend(jmsQueuePrefix + ".traceability", activity);
+		}
+		else {
+			sendInBatches(activity);
+		}
 	}
 
+	/**
+	 * A large activity object (with too many changes) should be messaged in batches.
+	 * @param activity
+	 */
+	void sendInBatches(Activity activity) {
+		int changeListSize = activity.getChanges().size();
+		logger.info("Number of changes (concept activities) is {} and is larger than max ({}).", changeListSize, maxConceptActiviesPerMessage );
+
+		Iterator<List<Activity.ConceptActivity>> activityListItr = Iterables.partition(activity.getChanges(), maxConceptActiviesPerMessage).iterator();
+		while (activityListItr.hasNext()) {
+			Activity activityChunk = new Activity(activity.getUserId(), activity.getBranchPath(),
+			activity.getCommitTimestamp(), activity.getSourceBranch(), activity.getActivityType());
+			activityChunk.setChanges(activityListItr.next());
+			jmsTemplate.convertAndSend(jmsQueuePrefix + ".traceability", activityChunk);
+		}
+	}
 }
