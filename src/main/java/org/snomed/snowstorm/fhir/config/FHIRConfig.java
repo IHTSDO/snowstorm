2a2bd667bef1968fb74a5022f0fafc30c2408950
Kai Kewley <kaikewley@gmail.com>
2022-08-30 16:58:54 +0100

    ISTO-59 Ability to load code systems from packages

--- src/main/java/org/snomed/snowstorm/fhir/config/FHIRConfig.java
+++ src/main/java/org/snomed/snowstorm/fhir/config/FHIRConfig.java
@@ -1,14 +1,22 @@
 package org.snomed.snowstorm.fhir.config;
 
+import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.rest.api.EncodingEnum;
+import org.snomed.snowstorm.fhir.services.FHIRAddBundleServlet;
 import org.snomed.snowstorm.fhir.services.HapiParametersMapper;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.servlet.MultipartConfigElement;
+import java.io.IOException;
+import java.nio.file.Files;
+
 @Configuration
 public class FHIRConfig {
-	
+
+	private static final int MB_IN_BYTES = 1024 * 1024;
+
 	@Bean
 	public HapiParametersMapper hapiParametersMapper() {
 		return new HapiParametersMapper();
@@ -17,12 +25,25 @@ public class FHIRConfig {
 	@Bean
 	public ServletRegistrationBean<HapiRestfulServlet> hapi() {
 		HapiRestfulServlet hapiServlet = new HapiRestfulServlet();
-		
+
 		ServletRegistrationBean<HapiRestfulServlet> servletRegistrationBean = new ServletRegistrationBean<>(hapiServlet, "/fhir/*");
 		hapiServlet.setServerName("Snowstorm FHIR Server");
 		hapiServlet.setServerVersion(getClass().getPackage().getImplementationVersion());
 		hapiServlet.setDefaultResponseEncoding(EncodingEnum.JSON);
 		return servletRegistrationBean;
+	}
+
+	@Bean
+	public ServletRegistrationBean<FHIRAddBundleServlet> addBundleServlet() throws IOException {
+		ServletRegistrationBean<FHIRAddBundleServlet> registrationBean = new ServletRegistrationBean<>(new FHIRAddBundleServlet(), "/fhir-admin/addBundle");
+		registrationBean.setMultipartConfig(
+				new MultipartConfigElement(Files.createTempDirectory("fhir-bundle-upload").toFile().getAbsolutePath(), MB_IN_BYTES * 200, MB_IN_BYTES * 200, 0));
+		return registrationBean;
+	}
+
+	@Bean
+	public FhirContext fhirContext() {
+		return FhirContext.forR4();
 	}
 
 }
