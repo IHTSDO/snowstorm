beba76343b65fb69c1ec1755dea8f6b84c6e4fdd
CoderMChu <mch@ihtsdo.org>
2022-09-13 15:40:49 +0100

    MAINT-2044 Remove commented out code in WebConfig.

--- src/main/java/org/snomed/snowstorm/config/WebConfig.java
+++ src/main/java/org/snomed/snowstorm/config/WebConfig.java
@@ -14,25 +14,4 @@ public class WebConfig implements WebMvcConfigurer {
 	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
 		converters.add(new ItemsPageCSVConverter());
 	}
-
-//	// For fixing springfox issue https://github.com/springfox/springfox/issues/3462
-//	@Bean
-//	public WebMvcEndpointHandlerMapping webEndpointServletHandlerMapping(WebEndpointsSupplier webEndpointsSupplier, ServletEndpointsSupplier servletEndpointsSupplier, ControllerEndpointsSupplier controllerEndpointsSupplier, EndpointMediaTypes endpointMediaTypes, CorsEndpointProperties corsProperties, WebEndpointProperties webEndpointProperties, Environment environment) {
-//		List<ExposableEndpoint<?>> allEndpoints = new ArrayList();
-//		Collection<ExposableWebEndpoint> webEndpoints = webEndpointsSupplier.getEndpoints();
-//		allEndpoints.addAll(webEndpoints);
-//		allEndpoints.addAll(servletEndpointsSupplier.getEndpoints());
-//		allEndpoints.addAll(controllerEndpointsSupplier.getEndpoints());
-//		String basePath = webEndpointProperties.getBasePath();
-//		EndpointMapping endpointMapping = new EndpointMapping(basePath);
-//		boolean shouldRegisterLinksMapping = this.shouldRegisterLinksMapping(webEndpointProperties, environment, basePath);
-//		return new WebMvcEndpointHandlerMapping(endpointMapping, webEndpoints, endpointMediaTypes, corsProperties.toCorsConfiguration(), new EndpointLinksResolver(allEndpoints, basePath), shouldRegisterLinksMapping, null);
-//	}
-//
-//
-//	private boolean shouldRegisterLinksMapping(WebEndpointProperties webEndpointProperties, Environment environment, String basePath) {
-//		return webEndpointProperties.getDiscovery().isEnabled() && (StringUtils.hasText(basePath) || ManagementPortType.get(environment).equals(ManagementPortType.DIFFERENT));
-//	}
-
-
 }
