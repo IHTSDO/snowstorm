9d50efd6395d7d7d8d1cb9d4b2be221fcb9216a3
Kai Kewley <kaikewley@gmail.com>
2022-11-18 15:42:23 +0000

    FHIR: Remove redundant code since HTML interceptor

--- src/main/java/org/snomed/snowstorm/fhir/config/RootInterceptor.java
+++ src/main/java/org/snomed/snowstorm/fhir/config/RootInterceptor.java
@@ -1,24 +1,17 @@
 package org.snomed.snowstorm.fhir.config;
 
-import java.io.InputStream;
-import java.util.Collections;
-import java.util.Enumeration;
-
-import javax.servlet.http.*;
-
+import ca.uhn.fhir.context.ConfigurationException;
+import ca.uhn.fhir.rest.server.interceptor.InterceptorAdapter;
+import com.amazonaws.util.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestWrapper;
 
-import com.amazonaws.util.IOUtils;
-
-import ca.uhn.fhir.context.ConfigurationException;
-import ca.uhn.fhir.rest.server.interceptor.InterceptorAdapter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.InputStream;
 
 public class RootInterceptor extends InterceptorAdapter {
-	
-	public static String ACCEPT = "Accept";
 	
 	private final Logger logger = LoggerFactory.getLogger(getClass());
 
@@ -39,53 +32,10 @@ public class RootInterceptor extends InterceptorAdapter {
 				IOUtils.copy(ios, response.getOutputStream());
 				return false;
 			}
-			
-			//If we detect headers that imply a browser client, then we'll tweak
-			//that to return json by default, since we don't have an html response available
-			if (request.getHeader("Accept") != null && request.getHeader("Accept").contains("text/html")) {
-				if (request instanceof SecurityContextHolderAwareRequestWrapper) {
-					HttpServletRequest unwrappedRequest = (HttpServletRequest)((SecurityContextHolderAwareRequestWrapper)request).getRequest();
-					MutableServletRequest mutableRequest = new MutableServletRequest(unwrappedRequest);
-					mutableRequest.putAcceptHeader("application/json");
-					((SecurityContextHolderAwareRequestWrapper)request).setRequest(mutableRequest);
-				} else {
-					logger.warn("Incomptabile request object received: " + request.getClass().getSimpleName());
-				}
-			}
 		} catch (Exception e) {
 			logger.error("Failed to intercept request", e);
 		}
 		return true;
 	}
 	
-	class MutableServletRequest extends HttpServletRequestWrapper {
-		HttpServletRequest request;
-		String acceptHeader;
-		
-		MutableServletRequest (HttpServletRequest request) {
-			super(request);
-			this.request = request;
-			this.acceptHeader = request.getHeader(ACCEPT);
-		}
-		
-		@Override
-		public String getHeader(String name) {
-			if (name.equals(ACCEPT)) {
-				return acceptHeader;
-			}
-			return request.getHeader(name);
-		}
-		
-		public void putAcceptHeader(String acceptHeader) {
-			this.acceptHeader = acceptHeader;
-		}
-		
-		@Override
-		public Enumeration<String> getHeaders(String name) {
-			if (name.equals(ACCEPT)) {
-				return Collections.enumeration(Collections.singleton(acceptHeader));
-			}
-			return request.getHeaders(name);
-		}
-	}
 }
