505eae24e68dadb6ed5c2e7ea29f2454043d9666
Kai Kewley <kaikewley@gmail.com>
2022-11-28 15:35:56 +0000

    ISTO-67: Fix ECL util function parsing "MINUS"
    Also fix the unit test - preventing the serialisation of null values caused the unit test to fail until the code was fixed.

--- src/test/java/org/snomed/snowstorm/ecl/deserializer/ECLModelDeserializerServiceTest.java
+++ src/test/java/org/snomed/snowstorm/ecl/deserializer/ECLModelDeserializerServiceTest.java
@@ -1,5 +1,6 @@
 package org.snomed.snowstorm.ecl.deserializer;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
@@ -20,6 +21,7 @@ class ECLModelDeserializerServiceTest {
 	public ECLModelDeserializerServiceTest() {
 		eclQueryBuilder = new ECLQueryBuilder(new SECLObjectFactory());
 		objectMapper = new ObjectMapper();
+		objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
 		eclModelDeserializerService = new ECLModelDeserializerService();
 	}
 
