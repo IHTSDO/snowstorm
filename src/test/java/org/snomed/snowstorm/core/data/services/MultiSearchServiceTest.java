b85055fd902a3572afdb466c8d4e9ae74f4898ac
CoderMChu <mch@ihtsdo.org>
2022-12-08 13:37:10 +0000

    MAINT-2100 Remove unnecessary extension upgrade

--- src/test/java/org/snomed/snowstorm/core/data/services/MultiSearchServiceTest.java
+++ src/test/java/org/snomed/snowstorm/core/data/services/MultiSearchServiceTest.java
@@ -54,7 +54,6 @@ class MultiSearchServiceTest extends AbstractTest {
 		CodeSystem codeSystemBE = new CodeSystem("SNOMEDCT-BE", "MAIN/SNOMEDCT-BE");
 		codeSystemService.createCodeSystem(codeSystemBE);
 		testUtil.createConceptWithPathIdAndTerm("MAIN/SNOMEDCT-BE", "123123404684003", "Some finding");
-		codeSystemUpgradeService.upgrade(null, codeSystemBE, 20190731, true);
 
 		assertEquals("Only one found because SNOMEDCT-BE code system is not versioned yet", 1, runSearch(term).getTotalElements());
 		codeSystemService.createVersion(codeSystemBE, 20190931, "");
