c55cba5e6272eb2f29c3cb508d9a0e15fcfdba71
Kai Kewley <kaikewley@gmail.com>
2023-01-18 09:21:47 +0000

    ISTO-65 Add create/update FHIR ConceptMap

--- src/main/java/org/snomed/snowstorm/fhir/repositories/FHIRConceptMapRepository.java
+++ src/main/java/org/snomed/snowstorm/fhir/repositories/FHIRConceptMapRepository.java
@@ -3,6 +3,9 @@ package org.snomed.snowstorm.fhir.repositories;
 import org.snomed.snowstorm.fhir.domain.FHIRConceptMap;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
+import java.util.List;
+
 public interface FHIRConceptMapRepository extends ElasticsearchRepository<FHIRConceptMap, String> {
 
+	List<FHIRConceptMap> findAllByUrl(String url);
 }
