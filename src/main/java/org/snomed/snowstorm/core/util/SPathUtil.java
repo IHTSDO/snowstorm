4397addb3a68697fa0941beb6c9da8e0c639cbb9
Darryn McGaw <dmcg@ihtsdo.org>
2023-05-16 17:05:51 +0100

    MAINT-1038 Create SPathUtil incl tests

---
+++ src/main/java/org/snomed/snowstorm/core/util/SPathUtil.java
@@ -0,0 +1,41 @@
+package org.snomed.snowstorm.core.util;
+
+import io.kaicode.elasticvc.api.PathUtil;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+// Snowstorm PathUtil has domain specific concepts irrelevant to the ElasticVC library.
+public class SPathUtil {
+    /**
+     * Return the given Branch path's ancestors. The given Branch path is not included in the ancestors,
+     * and processing stops at the CodeSystem level, i.e. processing an Extension's Branch path will stop at MAIN/SNOMEDCT-XX.
+     *
+     * @param branchPath Find ancestors for this Branch path.
+     * @return The given Branch path's ancestors.
+     */
+    public static List<String> getAncestors(String branchPath) {
+        if (branchPath == null || !branchPath.startsWith("MAIN")) {
+            throw new IllegalArgumentException("branchPath is invalid; cannot find ancestors.");
+        }
+
+        if (isCodeSystemBranch(branchPath)) {
+            return Collections.emptyList();
+        }
+
+        List<String> ancestors = new ArrayList<>();
+        String parent = PathUtil.getParentPath(branchPath);
+        ancestors.add(parent);
+        while (!isCodeSystemBranch(parent)) {
+            parent = PathUtil.getParentPath(parent);
+            ancestors.add(parent);
+        }
+
+        return ancestors;
+    }
+
+    private static boolean isCodeSystemBranch(String branchPath) {
+        return branchPath.equals("MAIN") || branchPath.startsWith("SNOMEDCT-", branchPath.lastIndexOf("/") + 1);
+    }
+}
