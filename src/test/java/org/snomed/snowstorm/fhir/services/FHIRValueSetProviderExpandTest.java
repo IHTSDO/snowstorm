50a6eafd7ba68857501fc52f1e832e4c696256d5
Kai Kewley <kaikewley@gmail.com>
2023-06-05 10:17:32 +0100

    ISTO-94 Fix extensional expand inactive flag.

--- src/test/java/org/snomed/snowstorm/fhir/services/FHIRValueSetProviderExpandTest.java
+++ src/test/java/org/snomed/snowstorm/fhir/services/FHIRValueSetProviderExpandTest.java
@@ -1,6 +1,7 @@
 package org.snomed.snowstorm.fhir.services;
 
 import ca.uhn.fhir.rest.api.MethodOutcome;
+import org.hl7.fhir.r4.model.ValueSet;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
@@ -8,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FHIRValueSetProviderExpandTest extends AbstractFHIRTest {
 
@@ -38,11 +40,44 @@ public class FHIRValueSetProviderExpandTest extends AbstractFHIRTest {
 			"    ]\n" +
 			"}";
 
+	private static final String VALUE_SET_WITH_INACTIVE_CODE = "{\n" +
+			"    \"resourceType\": \"Parameters\",\n" +
+			"    \"parameter\": [\n" +
+			"        {\n" +
+			"            \"name\": \"valueSet\",\n" +
+			"            \"resource\": {\n" +
+			"                \"resourceType\": \"ValueSet\",\n" +
+			"                \"compose\": {\n" +
+			"                    \"include\": [\n" +
+			"                        {\n" +
+			"                            \"system\": \"http://snomed.info/sct\",\n" +
+			"                            \"concept\": [ { \"code\": \"" + sampleInactiveSCTID + "\" } ]" +
+			"                        }\n" +
+			"                    ]\n" +
+			"                }\n" +
+			"            }\n" +
+			"        }\n" +
+			"    ]\n" +
+			"}";
+
 	@Test
 	public void testExpandThrowsFHIROperationExceptionIfVersionUsedWhilePerformingPOSTOperation() {
 		HttpEntity<String> request = new HttpEntity<>(VALUE_SET_EXPANSION_POST_BODY_WITH_VERSION, headers);
 		ResponseEntity<MethodOutcome> response = restTemplate.exchange(baseUrl + "/ValueSet/$expand", HttpMethod.POST, request, MethodOutcome.class);
 		assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+	}
+
+	@Test
+	public void testExpandInactiveCodeViaPOST() {
+		HttpEntity<String> request = new HttpEntity<>(VALUE_SET_WITH_INACTIVE_CODE, headers);
+		ResponseEntity<String> response = restTemplate.exchange(baseUrl + "/ValueSet/$expand", HttpMethod.POST, request, String.class);
+		ValueSet valueSet = fhirJsonParser.parseResource(ValueSet.class, response.getBody());
+
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		ValueSet.ValueSetExpansionContainsComponent concept = valueSet.getExpansion().getContains().get(0);
+		assertEquals("http://snomed.info/sct", concept.getSystem());
+		assertEquals("60363000", concept.getCode());
+		assertTrue(concept.getInactive());
 	}
 
 }
