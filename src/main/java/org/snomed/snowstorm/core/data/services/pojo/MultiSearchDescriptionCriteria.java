c8e6fa0298523457c633f1eb518c93ac109b6d8a
RichardAWood <rickawood@gmail.com>
2023-08-07 09:08:02 -0700

    MAINT-2282 Add multi search changes required by RT2 to snowstorm

---
+++ src/main/java/org/snomed/snowstorm/core/data/services/pojo/MultiSearchDescriptionCriteria.java
@@ -0,0 +1,43 @@
+package org.snomed.snowstorm.core.data.services.pojo;
+
+import org.snomed.snowstorm.rest.pojo.MultibranchDescriptionSearchRequest;
+
+import java.util.Set;
+
+public class MultiSearchDescriptionCriteria extends DescriptionCriteria{
+
+	private Set<Long> conceptIds;
+	private String ecl;
+	private MultibranchDescriptionSearchRequest includeBranches;
+
+	public MultiSearchDescriptionCriteria() {
+		super();
+	}
+
+	public MultiSearchDescriptionCriteria conceptIds(Set<Long> conceptIds) {
+		this.conceptIds = conceptIds;
+		return this;
+	}
+
+	public Set<Long> getConceptIds() {
+		return conceptIds;
+	}
+	
+	public MultiSearchDescriptionCriteria ecl(String ecl) {
+		this.ecl = ecl;
+		return this;
+	}
+	
+	public String getEcl() {
+		return ecl;
+	}
+	
+	public MultiSearchDescriptionCriteria includeBranches(MultibranchDescriptionSearchRequest includeBranches) {
+		this.includeBranches = includeBranches;
+		return this;
+	}
+	
+	public MultibranchDescriptionSearchRequest getIncludeBranches() {
+		return includeBranches;
+	}
+}
