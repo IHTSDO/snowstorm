b099682a14cfbda405567bbeee3b4cba002db4d9
CoderMChu <mch@ihtsdo.org>
2024-01-09 14:56:59 +0000

    PIP-468 Update Elasticsearch version to 8.11.1

--- src/test/java/org/snomed/snowstorm/TestConfig.java
+++ src/test/java/org/snomed/snowstorm/TestConfig.java
@@ -32,7 +32,7 @@ import jakarta.annotation.PostConstruct;
 		})
 public class TestConfig extends Config {
 
-	private static final String ELASTIC_SEARCH_SERVER_VERSION = "8.7.1";
+	private static final String ELASTIC_SEARCH_SERVER_VERSION = "8.11.1";
 
 	// set it to true to use local instance instead of test container
 	static final boolean useLocalElasticsearch = false;
