21c9c8fc1ce647e3614134257d8fcfa685870c5c
Kai Kewley <kaikewley@gmail.com>
2024-08-01 11:59:34 +0100

    ISTO-122 ConceptMap upload via FHIR Package

--- src/main/java/org/snomed/snowstorm/fhir/repositories/FHIRConceptMapRepository.java
+++ src/main/java/org/snomed/snowstorm/fhir/repositories/FHIRConceptMapRepository.java
@@ -3,6 +3,10 @@ package org.snomed.snowstorm.fhir.repositories;
 import org.snomed.snowstorm.fhir.domain.FHIRConceptMap;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
+import java.util.Collection;
+
 public interface FHIRConceptMapRepository extends ElasticsearchRepository<FHIRConceptMap, String> {
+
+	Collection<FHIRConceptMap> findAllByUrl(String url);
 
 }
