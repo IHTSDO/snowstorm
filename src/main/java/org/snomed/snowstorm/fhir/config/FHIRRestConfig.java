5593f32a729e018367a7247e47a2b053a603cae8
Kai Kewley <kaikewley@gmail.com>
2024-10-25 19:08:45 +0300

    MAINT-2822 Fix TerminologyCapability statement.

--- src/main/java/org/snomed/snowstorm/fhir/config/FHIRRestConfig.java
+++ src/main/java/org/snomed/snowstorm/fhir/config/FHIRRestConfig.java
@@ -1,8 +1,18 @@
 package org.snomed.snowstorm.fhir.config;
 
+import ca.uhn.fhir.rest.api.Constants;
 import ca.uhn.fhir.rest.api.EncodingEnum;
+import ca.uhn.fhir.rest.api.server.RequestDetails;
 import ca.uhn.fhir.rest.server.interceptor.ResponseHighlighterInterceptor;
+import org.hl7.fhir.instance.model.api.IBaseConformance;
+import org.hl7.fhir.r4.model.CapabilityStatement;
+import org.hl7.fhir.r4.model.StringType;
+import org.hl7.fhir.r4.model.TerminologyCapabilities;
+import org.snomed.snowstorm.core.data.services.CodeSystemService;
+import org.snomed.snowstorm.fhir.services.FHIRCodeSystemService;
 import org.snomed.snowstorm.fhir.services.FHIRLoadPackageServlet;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +20,8 @@ import org.springframework.context.annotation.Configuration;
 import jakarta.servlet.MultipartConfigElement;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class FHIRRestConfig {
@@ -17,16 +29,29 @@ public class FHIRRestConfig {
 	private static final int MB_IN_BYTES = 1024 * 1024;
 
 	@Bean
-	public ServletRegistrationBean<HapiRestfulServlet> hapi() {
-		HapiRestfulServlet hapiServlet = new HapiRestfulServlet();
+	public ServletRegistrationBean<HapiRestfulServlet> hapi(
+			@Autowired(required = false) BuildProperties buildProperties,
+			@Autowired FHIRCodeSystemService codeSystemService) {
+
+		HapiRestfulServlet hapiServlet = new HapiRestfulServlet(buildProperties, codeSystemService);
 
 		ServletRegistrationBean<HapiRestfulServlet> servletRegistrationBean = new ServletRegistrationBean<>(hapiServlet, "/fhir/*");
-		hapiServlet.setServerName("Snowstorm X FHIR Server");
-		hapiServlet.setServerVersion(getClass().getPackage().getImplementationVersion());
+		hapiServlet.setServerName("SnowstormX FHIR Server");
+		hapiServlet.setServerVersion(buildProperties != null ? buildProperties.getVersion() : "development");
 		hapiServlet.setDefaultResponseEncoding(EncodingEnum.JSON);
 
-		ResponseHighlighterInterceptor interceptor = new ResponseHighlighterInterceptor();
-		hapiServlet.registerInterceptor(interceptor);
+		hapiServlet.registerInterceptor(new ResponseHighlighterInterceptor() {
+			@Override
+			public void capabilityStatementGenerated(RequestDetails theRequestDetails, IBaseConformance theCapabilityStatement) {
+				if (theCapabilityStatement instanceof CapabilityStatement statement) {
+					statement.setPublisherElement(new StringType("SNOMED International"));
+					statement.setFormat(new ArrayList<>());
+					statement.addFormat(Constants.CT_FHIR_JSON_NEW);
+					statement.addFormat(Constants.CT_FHIR_XML_NEW);
+					super.capabilityStatementGenerated(theRequestDetails, theCapabilityStatement);
+				}
+			}
+		});
 
 		return servletRegistrationBean;
 	}
