9bd694c0d5ddedf7e261f178287c9ec8698c4411
CoderMChu <mch@ihtsdo.org>
2025-02-28 07:01:16 +0000

    PIP-677 Add concepts-lookup index for member of query

--- src/main/java/org/snomed/snowstorm/core/data/repositories/RefsetConceptsLookupRepository.java
+++
@@ -1,7 +0,0 @@
-package org.snomed.snowstorm.core.data.repositories;
-
-import org.snomed.snowstorm.core.data.domain.RefsetConceptsLookup;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface RefsetConceptsLookupRepository extends ElasticsearchRepository<RefsetConceptsLookup, String> {
-}
