784c2a065e71f834cb9d46a7c9d266dfe824c266
CoderMChu <mch@ihtsdo.org>
2025-04-08 11:20:56 +0100

    PIP-680 Use concepts lookup instead of prefetch member of query results for refined expression constraint

--- src/main/java/org/snomed/snowstorm/ecl/domain/RefinementBuilder.java
+++ src/main/java/org/snomed/snowstorm/ecl/domain/RefinementBuilder.java
@@ -27,4 +27,8 @@ public interface RefinementBuilder {
 	void inclusionFilterRequired();
 
 	boolean isInclusionFilterRequired();
+
+	void setShouldPrefetchMemberOfQueryResults(boolean prefetchMemberOfQueryResults);
+
+	Boolean shouldPrefetchMemberOfQueryResults();
 }
