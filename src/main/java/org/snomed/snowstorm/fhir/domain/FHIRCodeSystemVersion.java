1cb2ef257ff05d66688933c3aa17314415bf508a
guiste10 <guiste10@gmail.com>
2025-05-02 15:55:57 +0200

    MAINT-2880 Align implementation with deprecated group testcases

--- src/main/java/org/snomed/snowstorm/fhir/domain/FHIRCodeSystemVersion.java
+++ src/main/java/org/snomed/snowstorm/fhir/domain/FHIRCodeSystemVersion.java
@@ -56,6 +56,9 @@ public class FHIRCodeSystemVersion {
 	@Field(type = FieldType.Boolean)
 	private boolean compositional;
 
+	@Field(type = FieldType.Boolean)
+	private boolean experimental;
+
 	@Field(type = FieldType.Keyword)
 	private String content;
 
@@ -89,6 +92,7 @@ public class FHIRCodeSystemVersion {
 			this.id = codeSystem.getId().replace("CodeSystem/", "");
 		}
 		version = codeSystem.getVersion();
+		experimental = codeSystem.getExperimental();
 		date = codeSystem.getDate();
 		title = codeSystem.getTitle();
 		language = codeSystem.getLanguage();
@@ -301,6 +305,10 @@ public class FHIRCodeSystemVersion {
 
 	public void setCompositional(boolean compositional) {
 		this.compositional = compositional;
+	}
+
+	public boolean isExperimental() {
+		return experimental;
 	}
 
 	public String getContent() {
