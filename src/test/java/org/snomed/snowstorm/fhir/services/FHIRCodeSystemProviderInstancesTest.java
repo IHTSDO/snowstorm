41863b1f598a8063c72f78db73a79f49c724d192
Kai Kewley <kaikewley@gmail.com>
2025-07-31 11:20:55 +0100

    MAINT-2877 Fix CodeSystem sorting test.

--- src/test/java/org/snomed/snowstorm/fhir/services/FHIRCodeSystemProviderInstancesTest.java
+++ src/test/java/org/snomed/snowstorm/fhir/services/FHIRCodeSystemProviderInstancesTest.java
@@ -4,9 +4,13 @@ package org.snomed.snowstorm.fhir.services;
 import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.Bundle.BundleEntryComponent;
 import org.hl7.fhir.r4.model.CodeSystem;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -38,19 +42,32 @@ class FHIRCodeSystemProviderInstancesTest extends AbstractFHIRTest {
 	
 	@Test
 	void testCodeSystemRecoverySorted() {
-		String url = baseUrl + "/CodeSystem?_sort=title,-date";
+		assertEquals("[" +
+						"ICD-10|null, " +
+						"SNOMED CT release 2019-01-31|20190131, " +
+						"SNOMED CT release 2019-07-31|20190731" +
+				"]", getCodeSystemsTitleDate(baseUrl + "/CodeSystem?_sort=title,-date").toString());
+
+		assertEquals("[" +
+				"SNOMED CT release 2019-07-31|20190731, " +
+				"SNOMED CT release 2019-01-31|20190131, " +
+				"ICD-10|null" +
+				"]", getCodeSystemsTitleDate(baseUrl + "/CodeSystem?_sort=-title,date").toString());
+	}
+
+	private @NotNull List<String> getCodeSystemsTitleDate(String url) {
 		ResponseEntity<String> response = this.restTemplate.exchange(url, HttpMethod.GET, defaultRequestEntity, String.class);
 		expectResponse(response, 200);
 		Bundle bundle = fhirJsonParser.parseResource(Bundle.class, response.getBody());
 		assertNotNull(bundle.getEntry());
-		assertEquals(4, bundle.getEntry().size());
-		for (BundleEntryComponent entry : bundle.getEntry()) {
-			CodeSystem cs = (CodeSystem)(entry.getResource());
-			String title = cs.getTitle();
-			assertTrue(cs.getTitle().contains("SNOMED CT") || cs.getTitle().contains("ICD-10"), () -> "Found title " + cs.getTitle());
-		}
+		assertEquals(3, bundle.getEntry().size());
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
+		return bundle.getEntry().stream().map(entry -> {
+			CodeSystem codeSystem = (CodeSystem) (entry.getResource());
+			return "%s|%s".formatted(codeSystem.getTitle(), codeSystem.getDate() != null ? dateFormat.format(codeSystem.getDate()) : "null");
+		}).toList();
 	}
-	
+
 	@Test
 	void testCodeSystemRecoverySortedExpectedFail() {
 		String url = baseUrl + "/CodeSystem?_sort=foo,-bar";
