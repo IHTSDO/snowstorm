45997b4f185093fd1c7fd4e21be92e531732cb98
CoderMChu <mch@ihtsdo.org>
2025-08-01 15:54:31 +0100

    MAINT-2175 Move fetchMdrsMembers method to ModuleDependnecyService

--- src/main/java/org/snomed/snowstorm/core/data/services/AdditionalDependencyUpdateService.java
+++ src/main/java/org/snomed/snowstorm/core/data/services/AdditionalDependencyUpdateService.java
@@ -5,36 +5,27 @@ import io.kaicode.elasticvc.domain.Commit;
 import io.kaicode.elasticvc.domain.Metadata;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.snomed.snowstorm.core.data.domain.Concepts;
 import org.snomed.snowstorm.core.data.domain.ReferenceSetMember;
 import org.snomed.snowstorm.core.util.SPathUtil;
-import org.springframework.data.elasticsearch.client.elc.NativeQueryBuilder;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 
-import static co.elastic.clients.elasticsearch._types.query_dsl.QueryBuilders.bool;
 import static io.kaicode.elasticvc.domain.Commit.CommitType.CONTENT;
 import static io.kaicode.elasticvc.domain.Commit.CommitType.REBASE;
-import static io.kaicode.elasticvc.helper.QueryHelper.termQuery;
-import static org.snomed.snowstorm.core.data.domain.SnomedComponent.Fields.ACTIVE;
 
 @Service
 public class AdditionalDependencyUpdateService extends ComponentService implements CommitListener {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
     private final VersionControlHelper versionControlHelper;
-    private final ElasticsearchOperations elasticsearchOperations;
     private final ModuleDependencyService moduleDependencyService;
     private final BranchService branchService;
 
-    public AdditionalDependencyUpdateService(VersionControlHelper versionControlHelper, ElasticsearchOperations elasticsearchOperations, 
-                                             ModuleDependencyService moduleDependencyService, BranchService branchService) {
+    public AdditionalDependencyUpdateService(VersionControlHelper versionControlHelper, ModuleDependencyService moduleDependencyService, BranchService branchService) {
         this.versionControlHelper = versionControlHelper;
-        this.elasticsearchOperations = elasticsearchOperations;
         this.moduleDependencyService = moduleDependencyService;
         this.branchService = branchService;
     }
@@ -53,7 +44,7 @@ public class AdditionalDependencyUpdateService extends ComponentService implemen
 
     private boolean hasMdrsChanges(Commit commit) {
         BranchCriteria criteria = versionControlHelper.getBranchCriteriaChangesAndDeletionsWithinOpenCommitOnly(commit);
-        Set<ReferenceSetMember> mdrsMembers = fetchMdrsMembers(criteria);
+        Set<ReferenceSetMember> mdrsMembers = moduleDependencyService.fetchMdrsMembers(criteria);
         return !mdrsMembers.isEmpty();
     }
     private void performUpdate(Commit commit) {
@@ -63,7 +54,7 @@ public class AdditionalDependencyUpdateService extends ComponentService implemen
             return;
         }
         BranchCriteria changesOnBranchCriteria = versionControlHelper.getChangesOnBranchIncludingOpenCommit(commit);
-        Set<ReferenceSetMember> allMdrsMembers = fetchMdrsMembers(changesOnBranchCriteria);
+        Set<ReferenceSetMember> allMdrsMembers = moduleDependencyService.fetchMdrsMembers(changesOnBranchCriteria);
         Map<String, String> moduleToEffectiveTime = mapDependentModuleToEffectiveTime(allMdrsMembers);
         Set<String> dependentBranches = resolveAdditionalDependentBranches(moduleToEffectiveTime);
 
@@ -73,22 +64,6 @@ public class AdditionalDependencyUpdateService extends ComponentService implemen
         } else {
             removeAllAdditionalDependencies(commit);
         }
-    }
-
-    private Set<ReferenceSetMember> fetchMdrsMembers(BranchCriteria criteria) {
-        Set<ReferenceSetMember> members = new HashSet<>();
-        try (var searchResults = elasticsearchOperations.searchForStream(
-                new NativeQueryBuilder()
-                        .withQuery(bool(b -> b
-                                .must(criteria.getEntityBranchCriteria(ReferenceSetMember.class))
-                                .must(termQuery(ACTIVE, true))
-                                .must(termQuery(ReferenceSetMember.Fields.REFSET_ID, Concepts.MODULE_DEPENDENCY_REFERENCE_SET)))
-                        )
-                        .withPageable(LARGE_PAGE)
-                        .build(), ReferenceSetMember.class)) {
-            searchResults.forEachRemaining(hit -> members.add(hit.getContent()));
-        }
-        return members;
     }
 
     private Map<String, String> mapDependentModuleToEffectiveTime(Set<ReferenceSetMember> members) {
