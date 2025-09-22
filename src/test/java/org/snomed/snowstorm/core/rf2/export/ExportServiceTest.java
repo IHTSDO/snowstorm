78def518e854b345c872da138b36e6e46e664155
Kai Kewley <kaikewley@gmail.com>
2025-09-22 16:20:31 +0100

    MAINT-2966 Export concrete additional relationships

--- src/test/java/org/snomed/snowstorm/core/rf2/export/ExportServiceTest.java
+++ src/test/java/org/snomed/snowstorm/core/rf2/export/ExportServiceTest.java
@@ -118,7 +118,11 @@ class ExportServiceTest extends AbstractTest {
 
 		// add concrete attribute MRCM range constraint for data type
 		createRangeConstraint("1142135004", "dec(>#0..)");
+		// Concrete inferred relationship
 		concept.addRelationship(new Relationship("125024", null, true, Concepts.CORE_MODULE, conceptId, "#100", 0, "1142135004", Concepts.INFERRED_RELATIONSHIP,
+				Concepts.EXISTENTIAL));
+		// Concrete Additional relationship
+		concept.addRelationship(new Relationship("125025", null, true, Concepts.CORE_MODULE, conceptId, "\"PPA23080/021/002\"", 0, "680041000220101", ADDITIONAL_RELATIONSHIP,
 				Concepts.EXISTENTIAL));
 		conceptService.create(concept, path);
 
@@ -307,13 +311,16 @@ class ExportServiceTest extends AbstractTest {
 			assertTrue(lines.contains("125022\t\t1\t900000000000207008\t123001\t100002\t0\t116680003\t900000000000011006\t900000000000451002"));
 			assertTrue(lines.contains("125023\t\t1\t900000000000207008\t123001\t100003\t0\t116680003\t900000000000227009\t900000000000451002"));
 
-			// Inferred (non-concrete) Relationships
+			// Inferred Concrete Relationships
 			ZipEntry concreteRelationships = zipInputStream.getNextEntry();
 			assertEquals("SnomedCT_Export/Delta/Terminology/sct2_RelationshipConcreteValues_Delta_US_20210801.txt", concreteRelationships.getName());
 			lines = getLines(zipInputStream);
-			assertEquals(2, lines.size());
+			assertEquals(3, lines.size());
 			assertEquals(RF2Constants.CONCRETE_RELATIONSHIP_HEADER, lines.get(0));
+			// Inferred type concrete relationship
 			assertTrue(lines.contains("125024\t\t1\t900000000000207008\t123001\t#100\t0\t1142135004\t900000000000011006\t900000000000451002"));
+			// Additional type concrete relationship
+			assertTrue(lines.contains("125025\t\t1\t900000000000207008\t123001\t\"PPA23080/021/002\"\t0\t680041000220101\t900000000000227009\t900000000000451002"));
 
 			// Identifier
 			ZipEntry identifierTypes = zipInputStream.getNextEntry();
