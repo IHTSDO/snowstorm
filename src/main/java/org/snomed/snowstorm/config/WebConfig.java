afe2eeb21b33a73cc95b0e442bf07fe3fd650293
Quyen Ly <quocquyenit@gmail.com>
2025-11-19 11:22:07 +0700

    PIP-657 Add RBAC logging to snowstorm for Admin endpoints

--- src/main/java/org/snomed/snowstorm/config/WebConfig.java
+++ src/main/java/org/snomed/snowstorm/config/WebConfig.java
@@ -3,8 +3,10 @@ package org.snomed.snowstorm.config;
 import org.snomed.snowstorm.rest.converter.ItemsPageCSVConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.snomed.snowstorm.rest.interceptor.RequestLoggingInterceptor;
 
 import java.util.List;
 
@@ -18,5 +20,12 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
 		converters.add(new ItemsPageCSVConverter());
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		// Register request logging interceptor for admin endpoints
+		registry.addInterceptor(new RequestLoggingInterceptor("Admin request"))
+				.addPathPatterns("/admin/**");
 	}
 }
