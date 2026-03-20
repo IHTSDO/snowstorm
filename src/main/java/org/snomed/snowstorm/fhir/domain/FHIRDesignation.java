c6e225db997b25552cbc9586977a7c6c44087396
Kai Kewley <kaikewley@gmail.com>
2026-03-20 14:00:46 +0000

    MAINT-2889 Unit test fixes.

--- src/main/java/org/snomed/snowstorm/fhir/domain/FHIRDesignation.java
+++ src/main/java/org/snomed/snowstorm/fhir/domain/FHIRDesignation.java
@@ -5,6 +5,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.hl7.fhir.r4.model.CodeSystem;
 import org.hl7.fhir.r4.model.Coding;
 import org.hl7.fhir.r4.model.ValueSet;
+import org.snomed.snowstorm.fhir.config.FHIRConstants;
 import org.snomed.snowstorm.core.data.domain.Description;
 
 import java.util.ArrayList;
@@ -91,7 +92,13 @@ public class FHIRDesignation {
 				String[] split = use.split("\\|");
 				return addKnownDisplays(new Coding(split[0], split[1], null));
 			} else {
-				return new Coding(null, use, null);
+				// Some serialized designations may lose the original system (e.g. only "display" is retained).
+				// Reconstruct the expected HL7 designation-usage system where possible.
+				if (FHIRConstants.DISPLAY.equals(use)) {
+					return addKnownDisplays(new Coding(FHIRConstants.HL7_CS_DESIGNATION_USAGE, use, null));
+				}
+				// For SNOMED description acceptability designations, some payloads may only retain the code.
+				return addKnownDisplays(new Coding(SNOMED_URI, use, null));
 			}
 		}
 		return null;
